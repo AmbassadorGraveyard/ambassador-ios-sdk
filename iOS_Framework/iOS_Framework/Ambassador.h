0975e9c5f45a1e5b7335d254edda3e4599cd704e
austinnb <austinbrewer@live.com>
2015-06-18 17:49:21 -0400

    Added the ambassador class

---
+++ iOS_Framework/iOS_Framework/Ambassador.h
@@ -0,0 +1,13 @@
+//
+//  Ambassador.h
+//  iOS_Framework
+//
+//  Created by Diplomat on 6/18/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ambassador : NSObject
+
+@end
---
+++ iOS_Framework/iOS_Framework/Ambassador.m
@@ -0,0 +1,13 @@
+//
+//  Ambassador.m
+//  iOS_Framework
+//
+//  Created by Diplomat on 6/18/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import "Ambassador.h"
+
+@implementation Ambassador
+
+@end
