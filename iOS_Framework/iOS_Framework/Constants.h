407d7e9edf8c8e282a1282b63e7bcb9ceb7e0224
austinnb <austinbrewer@live.com>
2015-06-19 09:44:50 -0400

    [#97324752] Created method stumps and started logging calls to them

--- iOS_Framework/iOS_Framework/Constants.h
+++ iOS_Framework/iOS_Framework/Constants.h
@@ -9,6 +9,12 @@
 #ifndef iOS_Framework_Constants_h
 #define iOS_Framework_Constants_h
 
+#ifdef DEBUG
+#	define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#	define DLog(...)
+#endif
+
 extern NSString * const AMB_IDENTIFY_NOTIFICATION_NAME;
 extern NSString * const AMB_IDENTIFY_STORAGE_KEY;
 
