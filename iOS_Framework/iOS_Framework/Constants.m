471f374eb9b065b88e05c592172009e05b9dcc73
austinnb <austinbrewer@live.com>
2015-06-22 10:36:32 -0400

    [#97323880] Initial set up of identify object

--- iOS_Framework/iOS_Framework/Constants.m
+++ iOS_Framework/iOS_Framework/Constants.m
@@ -17,4 +17,9 @@
 
 NSString * const AMB_IDENTIFY_NOTIFICATION_NAME = @"AMBIDENTNOTIF";
 NSString * const AMB_IDENTIFY_STORAGE_KEY = @"AMBIDENTSTORAGE";
+NSString * const AMB_IDENTIFY_URL = @"http://127.0.0.1:7999/augur.html?cbURL=ambassador:mylocation";
+NSString * const AMB_IDENTIFY_JS_VAR = @"JSONdata";
+NSString * const AMB_IDENTIFY_USER_DEFUALTS_KEY = @"AMBidentifyData";
+NSString * const AMB_IDENTIFY_SIGNAL_URL = @"ambassador";
 float const AMB_CONVERSION_FLUSH_TIME = 10.0;
+float const AMB_IDENTIFY_RETRY_TIME = 2.0;
