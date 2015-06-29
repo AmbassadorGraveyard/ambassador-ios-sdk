742b1530e31e6d54a765a1d296e4bfce30e785cd
austinnb <austinbrewer@live.com>
2015-06-29 11:38:55 -0400

    [#97324606] Switched to SQLite for persistant store

---
+++ iOS_Framework/iOS_Framework/Conversion.m
@@ -0,0 +1,13 @@
+//
+//  Conversion.m
+//  iOS_Framework
+//
+//  Created by Diplomat on 6/29/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import "Conversion.h"
+
+@implementation Conversion
+
+@end
---
+++ iOS_Framework/iOS_Framework/ConversionParameters.h
@@ -0,0 +1,13 @@
+//
+//  ConversionParameters.h
+//  iOS_Framework
+//
+//  Created by Diplomat on 6/29/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ConversionParameters : NSObject
+
+@end
