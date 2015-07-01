a0609bc3ce0f854bdc6992cf4d23903a31a57938
austinnb <austinbrewer@live.com>
2015-07-01 11:06:13 -0400

    [#97324606] Conversion with full available fingerrpint data send

--- iOS_Framework/iOS_Framework/Conversion.m
+++ iOS_Framework/iOS_Framework/Conversion.m
@@ -62,6 +62,11 @@
 - (void)registerConversionWithParameters:(ConversionParameters *)parameters
 {
     DLog();
+    
+    //Check that the conversion parameters meet the backend's requirements
+    //  * mbsy_revenue
+    //  * mbsy_campaign
+    //  * mbsy_email
     if (![parameters isValid])
     {
         [NSException raise:@"Invalid conversion parameters"
@@ -98,92 +103,103 @@
     DLog();
     NSDictionary *userDefaultsIdentify = [[NSUserDefaults standardUserDefaults] dictionaryForKey:AMB_IDENTIFY_USER_DEFUALTS_KEY];
     NSDictionary *userDefaultsInsights = [[NSUserDefaults standardUserDefaults] dictionaryForKey:AMB_INSIGHTS_USER_DEFAULTS_KEY];
+    
+    //Check if insights and identify data exist to send. Else don't send
     if (!userDefaultsInsights) { return; }
     
     [self.databaseQueue inDatabase:^(FMDatabase *db)
     {
-        DLog(@"In database queue");
+        DLog(@"Getting all database records");
         FMResultSet *resultSet = [db executeQuery:[NSString stringWithFormat:@"SELECT * FROM %@", AMB_CONVERSION_SQL_TABLE_NAME]];
         
         while ([resultSet next])
         {
-            NSMutableDictionary * fields = [NSMutableDictionary dictionaryWithDictionary: @{
-                                                                                            @"mbsy_campaign" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_campaign"]],
-                                                                                            @"mbsy_email" : [resultSet stringForColumn:@"mbsy_email"],
-                                                                                            @"mbsy_first_name" : [resultSet stringForColumn:@"mbsy_first_name"],
-                                                                                            @"mbsy_last_name" : [resultSet stringForColumn:@"mbsy_last_name"],
-                                                                                            @"mbsy_email_new_ambassador" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_email_new_ambassador"]],
-                                                                                            @"mbsy_uid" : [resultSet stringForColumn:@"mbsy_uid"],
-                                                                                            @"mbsy_custom1" : [resultSet stringForColumn:@"mbsy_custom1"],
-                                                                                            @"mbsy_custom2" : [resultSet stringForColumn:@"mbsy_custom2"],
-                                                                                            @"mbsy_custom3" : [resultSet stringForColumn:@"mbsy_custom3"],
-                                                                                            @"mbsy_auto_create" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_auto_create"]],
-                                                                                            @"mbsy_revenue" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_revenue"]],
-                                                                                            @"mbsy_deactivate_new_ambassador" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_deactivate_new_ambassador"]],
-                                                                                            @"mbsy_transaction_uid" : [resultSet stringForColumn:@"mbsy_transaction_uid"],
-                                                                                            @"mbsy_add_to_group_id" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_add_to_group_id"]],
-                                                                                            @"mbsy_event_data1" : [resultSet stringForColumn:@"mbsy_event_data1"],
-                                                                                            @"mbsy_event_data2" : [resultSet stringForColumn:@"mbsy_event_data2"],
-                                                                                            @"mbsy_event_data3" : [resultSet stringForColumn:@"mbsy_event_data3"],
-                                                                                            @"mbsy_is_approved" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_is_approved"]]
-                                                                                            }];
+            NSMutableDictionary * fields = [NSMutableDictionary dictionaryWithDictionary:
+                @{
+                    @"mbsy_campaign" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_campaign"]],
+                    @"mbsy_email" : [resultSet stringForColumn:@"mbsy_email"],
+                    @"mbsy_first_name" : [resultSet stringForColumn:@"mbsy_first_name"],
+                    @"mbsy_last_name" : [resultSet stringForColumn:@"mbsy_last_name"],
+                    @"mbsy_email_new_ambassador" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_email_new_ambassador"]],
+                    @"mbsy_uid" : [resultSet stringForColumn:@"mbsy_uid"],
+                    @"mbsy_custom1" : [resultSet stringForColumn:@"mbsy_custom1"],
+                    @"mbsy_custom2" : [resultSet stringForColumn:@"mbsy_custom2"],
+                    @"mbsy_custom3" : [resultSet stringForColumn:@"mbsy_custom3"],
+                    @"mbsy_auto_create" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_auto_create"]],
+                    @"mbsy_revenue" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_revenue"]],
+                    @"mbsy_deactivate_new_ambassador" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_deactivate_new_ambassador"]],
+                    @"mbsy_transaction_uid" : [resultSet stringForColumn:@"mbsy_transaction_uid"],
+                    @"mbsy_add_to_group_id" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_add_to_group_id"]],
+                    @"mbsy_event_data1" : [resultSet stringForColumn:@"mbsy_event_data1"],
+                    @"mbsy_event_data2" : [resultSet stringForColumn:@"mbsy_event_data2"],
+                    @"mbsy_event_data3" : [resultSet stringForColumn:@"mbsy_event_data3"],
+                    @"mbsy_is_approved" : [NSNumber numberWithInt:[resultSet intForColumn:@"mbsy_is_approved"]]
+                }];
             
-                NSDictionary * consumer = @{
-                                            @"UID" : userDefaultsIdentify[@"consumer"][@"UID"],
-                                            @"insights" : userDefaultsInsights
-                                            };
-                NSDictionary * device = @{
-                                          @"type" : userDefaultsIdentify[@"device"][@"type"],
-                                          @"ID" : userDefaultsIdentify[@"device"][@"ID"]
-                                          };
-                
-                [fields removeObjectForKey:@"insights"];
-                NSDictionary * payload = @{
-                                           @"fp" : @{
-                                                   @"consumer" : consumer,
-                                                   @"device" : device
-                                                   },
-                                           @"fields" : fields
-                                           };
-                
-                DLog(@"WE HAVE INSIGHTS DATA");
-                
-                // URL of the endpoint we're going to contact.
-                NSURL *url = [NSURL URLWithString:AMB_CONVERSION_URL];
-                
-                // Convert the dictionary into JSON data.
-                NSData *JSONData = [NSJSONSerialization dataWithJSONObject:payload
-                                                                   options:0
-                                                                     error:nil];
-                
-                // Create a POST request with our JSON as a request body.
-                NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-                request.HTTPMethod = @"POST";
-                [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-                [request setValue:AMB_MBSY_UNIVERSAL_ID forHTTPHeaderField:@"MBSY_UNIVERSAL_ID"];
-                [request setValue:AMB_AUTHORIZATION_TOKEN forHTTPHeaderField:@"Authorization"];
-                request.HTTPBody = JSONData;
-                
-                // Create a task.
-                NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request
-                                                                             completionHandler:^(NSData *data,
-                                                                                                 NSURLResponse *response,
-                                                                                                 NSError *error)
-                                              {
-                                                  if (!error)
-                                                  {
-                                                      NSLog(@"Status code: %ld", ((NSHTTPURLResponse *)response).statusCode);
-                                                      DLog(@"%@", [NSJSONSerialization JSONObjectWithData:data options:0 error:nil]);
-                                                      DLog(@"%i", [resultSet intForColumn:@"ID"]);
-                                                      [db executeQuery:@"Delete from conversions where ID = ?", [resultSet intForColumn:@"ID"]];
-                                                  }
-                                                  else
-                                                  {
-                                                      NSLog(@"Error: %@", error.localizedDescription);
-                                                  }
-                                              }];
-                
-                [task resume];
+            //Remove 'status' key from insights before sending to the backend
+            NSMutableDictionary *insightsDataCopy = [NSMutableDictionary dictionaryWithDictionary:userDefaultsInsights];
+            if ([insightsDataCopy objectForKey:@"status"])
+            {
+                [insightsDataCopy removeObjectForKey:@"status"];
+            }
+            
+            //Build the payload data to POST to the backend
+            NSDictionary * consumer = @{
+                                        @"UID" : userDefaultsIdentify[@"consumer"][@"UID"],
+                                        @"insights" : insightsDataCopy
+                                        };
+            NSDictionary * device = @{
+                                      @"type" : userDefaultsIdentify[@"device"][@"type"],
+                                      @"ID" : userDefaultsIdentify[@"device"][@"ID"]
+                                      };
+            
+            [fields removeObjectForKey:@"insights"];
+            NSDictionary * payload = @{
+                                       @"fp" : @{
+                                               @"consumer" : consumer,
+                                               @"device" : device
+                                               },
+                                       @"fields" : fields
+                                       };
+            
+            NSData *JSONData = [NSJSONSerialization dataWithJSONObject:payload
+                                                               options:0
+                                                                 error:nil];
+            
+            //Create the POST request
+            NSURL *url = [NSURL URLWithString:AMB_CONVERSION_URL];
+            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+            request.HTTPMethod = @"POST";
+            [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+            [request setValue:AMB_MBSY_UNIVERSAL_ID forHTTPHeaderField:@"MBSY_UNIVERSAL_ID"];
+            [request setValue:AMB_AUTHORIZATION_TOKEN forHTTPHeaderField:@"Authorization"];
+            request.HTTPBody = JSONData;
+        
+            //Get ID in order to remove upon sucessful network request
+            NSNumber * sql_ID = [NSNumber numberWithInt:[resultSet intForColumn:@"ID"]];
+        
+            NSURLSessionDataTask *task = [[NSURLSession sharedSession]
+                                          dataTaskWithRequest:request
+                                          completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+              {
+                  if (!error)
+                  {
+                      NSLog(@"Status code: %ld", ((NSHTTPURLResponse *)response).statusCode);
+                      
+                      //Check for 2xx status codes
+                      if (((NSHTTPURLResponse *)response).statusCode >= 200 &&
+                          ((NSHTTPURLResponse *)response).statusCode < 300)
+                      {
+                          DLog(@"Response from backend for record ID %@: %@", sql_ID, [NSJSONSerialization JSONObjectWithData:data options:0 error:nil]);
+                          [db executeUpdate:@"Delete from conversions where ID = ?",  sql_ID];
+                      }
+                  }
+                  else
+                  {
+                      NSLog(@"Error: %@", error.localizedDescription);
+                  }
+              }];
+            [task resume];
         }
     }];
 }
--- iOS_Framework_Test_App/iOS_Framework_Test_App/ViewController.m
+++ iOS_Framework_Test_App/iOS_Framework_Test_App/ViewController.m
@@ -21,9 +21,25 @@
     [super viewDidLoad];
     ConversionParameters *conversion = [[ConversionParameters alloc] init];
     conversion.mbsy_add_to_group_id = @134;
-    conversion.mbsy_revenue = 0;
+    conversion.mbsy_revenue = @1000;
     conversion.mbsy_campaign = @260;
-    conversion.mbsy_email = @"1954testimport@example.com";
+    conversion.mbsy_first_name = @"test";
+    conversion.mbsy_last_name = @"one";
+    conversion.mbsy_email_new_ambassador = @YES;
+    conversion.mbsy_uid = @"uiduiduiduiduiduiduid";
+    conversion.mbsy_custom1 = @"custom111";
+    conversion.mbsy_custom2 = @"custom222";
+    conversion.mbsy_custom3 = @"custom333";
+    conversion.mbsy_auto_create = @NO;
+    conversion.mbsy_deactivate_new_ambassador = @YES;
+    conversion.mbsy_transaction_uid = @"transuidtransuid";
+    conversion.mbsy_add_to_group_id = @123456789;
+    conversion.mbsy_event_data1 = @"eventdata1";
+    conversion.mbsy_event_data2 = @"eventdata2";
+    conversion.mbsy_event_data3 = @"eventdata3";
+    conversion.mbsy_is_approved = @NO;
+    
+    conversion.mbsy_email = @"anonymous_test_1578@example.com";
     [Ambassador registerConversion:conversion];
     [Ambassador presentRAFFromViewController:self];
     
@@ -35,7 +51,7 @@
     self.message = [[UILabel alloc] init];
     self.message.text = @"Isn't this the sweetest app ever?!?";
     self.message.textAlignment = NSTextAlignmentCenter;
-    self.message.backgroundColor = [UIColor whiteColor];
+    self.message.backgroundColor = [UIColor orangeColor];
     self.message.translatesAutoresizingMaskIntoConstraints = NO;
     self.message.alpha = 1.0f;
     self.message.textColor = [UIColor whiteColor];
