9bbc643f35272e0d86f3365c5a76ce94b6651f01
austinnb <austinbrewer@live.com>
2015-07-16 10:31:25 -0400

    [#99049194] Cleaned up code and logs for review

--- iOS_Framework/iOS_Framework/Constants.h
+++ iOS_Framework/iOS_Framework/Constants.h
@@ -15,11 +15,28 @@
 #	define DLog(...)
 #endif
 
+
+#pragma mark - Notifications
 extern NSString * const AMB_IDENTIFY_NOTIFICATION_NAME;
+
+
+
+#pragma mark - UserDefaults
 extern NSString * const AMB_IDENTIFY_USER_DEFAULTS_KEY;
-extern NSString * const AMB_AMBASSADOR_INFO_STORAGE_KEY;
+extern NSString * const AMB_AMBASSADOR_INFO_USER_DEFAULTS_KEY;
 extern NSString * const AMB_INSIGHTS_USER_DEFAULTS_KEY;
+
+
+
+#pragma mark - Ambassador Back End
 extern NSString * const AMB_MBSY_UNIVERSAL_ID;
 extern NSString * const AMB_AUTHORIZATION_TOKEN;
+
+
+
+#pragma mark - Pusher
+extern NSString * const AMB_PUSHER_KEY;
+extern NSString * const AMB_PUSHER_AUTHENTICATION_URL;
+
 
 #endif
