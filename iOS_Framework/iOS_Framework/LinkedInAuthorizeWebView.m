b502951e5e9db3f2adc43ddd69ffc3b4e20c2520
austinnb <austinbrewer@live.com>
2015-07-20 16:55:00 -0400

    [#97326528] Cleaned up code structure, added RAF service selector and contact sheets (iPhone and iPad)

---
+++ iOS_Framework/iOS_Framework/LinkedInAuthorizeWebView.m
@@ -0,0 +1,13 @@
+//
+//  LinkedInAuthorizeWebView.m
+//  iOS_Framework
+//
+//  Created by Diplomat on 7/16/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import "LinkedInAuthorizeWebView.h"
+
+@implementation LinkedInAuthorizeWebView
+
+@end
---
+++ iOS_Framework/iOS_Framework/RAFShareScreen.m
@@ -0,0 +1,13 @@
+//
+//  RAFShareScreen.m
+//  iOS_Framework
+//
+//  Created by Diplomat on 7/16/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import "RAFShareScreen.h"
+
+@implementation RAFShareScreen
+
+@end
