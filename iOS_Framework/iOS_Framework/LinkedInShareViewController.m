e95050629c037e63aafd3ffa2ceeeffc32ddb723
austinnb <austinbrewer@live.com>
2015-07-20 16:55:00 -0400

    [#97326528] Cleaned up code structure, added RAF service selector and contact sheets (iPhone and iPad)

---
+++ iOS_Framework/iOS_Framework/LinkedInShareViewController.m
@@ -0,0 +1,13 @@
+//
+//  LinkedInShareViewController.m
+//  iOS_Framework
+//
+//  Created by Diplomat on 7/17/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import "LinkedInShareViewController.h"
+
+@implementation LinkedInShareViewController
+
+@end
---
+++ iOS_Framework/iOS_Framework/RAFNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  RAFNavigationController.h
+//  iOS_Framework
+//
+//  Created by Diplomat on 7/16/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RAFNavigationController : UINavigationController
+
+@end
