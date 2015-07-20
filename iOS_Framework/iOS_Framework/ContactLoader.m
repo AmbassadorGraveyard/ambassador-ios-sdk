7d70b3b1b0c62dcfb39e5b185acddd6687d5e61a
austinnb <austinbrewer@live.com>
2015-07-20 16:55:00 -0400

    [#97326528] Cleaned up code structure, added RAF service selector and contact sheets (iPhone and iPad)

---
+++ iOS_Framework/iOS_Framework/ContactLoader.m
@@ -0,0 +1,205 @@
+//
+//  ContactLoader.m
+//  iOS_Framework
+//
+//  Created by Diplomat on 7/8/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import "ContactLoader.h"
+#import <AddressBook/AddressBook.h>
+#import "Constants.h"
+#import "Contact.h"
+
+@interface ContactLoader ()
+
+@property ABAddressBookRef addressBook;
+@property NSArray *allContacts;
+
+@end
+
+
+
+@implementation ContactLoader
+
+- (id)init
+{
+    if ([super init])
+    {
+        DLog();
+        [self setUp];
+    }
+    
+    return self;
+}
+
+- (void)setUp
+{
+    DLog();
+    //Initialize containers
+    self.emailAddresses = [[NSMutableArray alloc] init];
+    self.phoneNumbers = [[NSMutableArray alloc] init];
+    
+    //Set up address book
+    self.addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+    [self getPermissions];
+}
+
+- (void)getPermissions
+{
+    if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusDenied ||
+        ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusRestricted)
+    {
+        DLog(@"Don't have permission to access contacts");
+        //TODO: alert user that we need contact permission
+    }
+    else if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusAuthorized)
+    {
+        DLog(@"Already had permission to access contacts");
+        [self loadContacts];
+    }
+    else
+    {
+        DLog(@"Asking for permission to access contacts");
+        ABAddressBookRequestAccessWithCompletion(ABAddressBookCreateWithOptions(NULL, nil), ^(bool granted, CFErrorRef error) {
+            if (!granted)
+            {
+                DLog(@"Contact access permission request denied");
+                //TODO: alert user that we need contact permission
+            }
+            DLog(@"Contact access permission request granted");
+            [self loadContacts];
+        });
+    }
+}
+
+- (void)loadContacts
+{
+    DLog();
+    if (self.addressBook != nil)
+    {
+        ABRecordRef source = ABAddressBookCopyDefaultSource(self.addressBook);
+        self.allContacts = (__bridge NSArray *)ABAddressBookCopyArrayOfAllPeopleInSourceWithSortOrdering(self.addressBook, source, kABPersonFirstNameProperty);
+        for (NSUInteger i = 0; i < self.allContacts.count; ++i)
+        {
+            ABRecordRef person = (__bridge ABRecordRef)self.allContacts[i];
+            [self getEmailsForPerson:person];
+            [self getNumbersForPerson:person];
+        }
+        //DLog(@"Contact phone numbers: %@\nContact email addresses: %@", self.phoneNumbers, self.emailAddresses);
+    }
+}
+
+- (void)getNumbersForPerson:(ABRecordRef)person
+{
+    // Get name
+    NSDictionary *name = [self getNameForPerson:person];
+    
+    // Emails for contact
+    ABMultiValueRef emails = ABRecordCopyValue(person, kABPersonEmailProperty);
+    
+    // Cycle through emails
+    for (CFIndex j = 0; j < ABMultiValueGetCount(emails); ++j) {
+        
+        //String to store label of each email type
+        NSString *emailLabel = (__bridge NSString *)ABMultiValueCopyLabelAtIndex(emails, j);
+        
+        //Strip the charaters surrounding label type
+        emailLabel = [emailLabel stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"_.$!<>"]];
+        
+        NSString *email = (__bridge NSString *)ABMultiValueCopyValueAtIndex(emails, j);
+        
+        Contact *contact = [[Contact alloc] init];
+        contact.firstName = name[@"firstName"];
+        contact.lastName = name[@"lastName"];
+        contact.label = emailLabel;
+        contact.value = email;
+        
+        [self.emailAddresses addObject:contact];
+    }
+}
+
+- (void)getEmailsForPerson:(ABRecordRef)person
+{
+    // Get name
+    NSDictionary *name = [self getNameForPerson:person];
+    
+    // Phone numbers for contact
+    ABMultiValueRef phones = ABRecordCopyValue(person, kABPersonPhoneProperty);
+    
+    NSString *phoneLabel;
+    
+    // Cycle through phone numbers
+    for (CFIndex j = 0; j < ABMultiValueGetCount(phones); ++j)
+    {
+        //String to store lable of each number type
+        phoneLabel = (__bridge NSString *)ABMultiValueCopyLabelAtIndex(phones, j);
+        
+        //Check if it's mobile of iPhone
+        if ([phoneLabel isEqualToString:(NSString *)kABPersonPhoneMobileLabel] ||
+            [phoneLabel isEqualToString:(NSString *)kABPersonPhoneIPhoneLabel] ||
+            YES ) //TODO: remove YES
+        {
+            //Strip the charaters surrounding label type
+            phoneLabel = [phoneLabel stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"_.$!<>"]];
+            
+            //Get the number
+            NSString *number = (__bridge NSString *)ABMultiValueCopyValueAtIndex(phones, j);
+            
+            //Remove non-numeric characters from number string
+            NSCharacterSet *breakAtCharacters = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789"] invertedSet];
+            number = [[number componentsSeparatedByCharactersInSet:breakAtCharacters] componentsJoinedByString:@""];
+            
+            Contact *contact = [[Contact alloc] init];
+            contact.firstName = name[@"firstName"];
+            contact.lastName = name[@"lastName"];
+            contact.label = phoneLabel;
+            contact.value = [self formatPhoneNumber:number];
+            
+            [self.phoneNumbers addObject:contact];
+        }
+    }
+}
+
+- (NSDictionary *)getNameForPerson:(ABRecordRef)person
+{
+    NSString *firstName, *lastName;
+    firstName = (__bridge NSString *)ABRecordCopyValue(person, kABPersonFirstNameProperty);
+    lastName = (__bridge NSString *)ABRecordCopyValue(person, kABPersonLastNameProperty);
+    
+    return @{
+             @"firstName" : firstName,
+             @"lastName" : lastName
+             };
+}
+
+- (NSString *)formatPhoneNumber:(NSString *)number
+{
+    NSMutableString *returnNumber = [NSMutableString stringWithString:number];
+    if (number.length == 11)
+    {
+        //country code
+        [returnNumber insertString:@" " atIndex:1];
+        [returnNumber insertString:@"(" atIndex:2];
+        [returnNumber insertString:@")" atIndex:6];
+        [returnNumber insertString:@" " atIndex:7];
+        [returnNumber insertString:@"-" atIndex:11];
+    }
+    else if (number.length == 10)
+    {
+        // area code
+        [returnNumber insertString:@"(" atIndex:0];
+        [returnNumber insertString:@")" atIndex:4];
+        [returnNumber insertString:@" " atIndex:5];
+        [returnNumber insertString:@"-" atIndex:9];
+    }
+    else
+    {
+        // simple number
+        [returnNumber insertString:@"-" atIndex:3];
+    }
+    return returnNumber;
+}
+
+
+@end
