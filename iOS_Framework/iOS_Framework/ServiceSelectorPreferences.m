9876bb6092267dfd211ad79f62dfbf4af78843ed
austinnb <austinbrewer@live.com>
2015-07-28 15:26:28 -0400

    [#100019870] Polished contact seletors and added twitter and facebook modals

--- iOS_Framework/iOS_Framework/ServiceSelectorPreferences.m
+++ iOS_Framework/iOS_Framework/ServiceSelectorPreferences.m
@@ -10,4 +10,17 @@
 
 @implementation ServiceSelectorPreferences
 
+- (id)init
+{
+    if ([super init])
+    {
+        self.titleLabelText = @"Spread the word";
+        self.navBarTitle = @"Refer your friends";
+        self.descriptionLabelText = @"Refer a friend to get rewards";
+        self.defaultShareMessage = @"I'm a fan of this company, check them out!";
+    }
+    
+    return self;
+}
+
 @end
