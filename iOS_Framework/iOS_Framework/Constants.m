15dfe8b83ff33fb8b86bc6fd4fc36c5bbeb996b4
austinnb <austinbrewer@live.com>
2015-08-03 14:14:11 -0400

    [#100037272] Updated Readme to reflect changes done in migration to storyboard

--- iOS_Framework/iOS_Framework/Constants.m
+++ iOS_Framework/iOS_Framework/Constants.m
@@ -23,7 +23,7 @@ NSString * const AMB_MBSY_UNIVERSAL_ID = @"abfd1c89-4379-44e2-8361-ee7b87332e32"
 NSString * const AMB_AUTHORIZATION_TOKEN = @"UniversalToken bdb49d2b9ae24b7b6bc5da122370f3517f98336f";
 
 NSString * const AMB_PUSHER_KEY = @"8bd3fe1994164f9b83f6";
-
+ 
 NSString * const AMB_PUSHER_AUTHENTICATION_URL = @"https://dev-ambassador-api.herokuapp.com/auth/subscribe/";
 
 NSString * const AMB_FIRST_LAUNCH_USER_DEFAULTS_KEY = @"AMBFIRSTLAUNCHSTORAGE";
