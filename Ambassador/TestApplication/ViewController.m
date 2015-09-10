704b1d4057255553aaf63aa890710bdc33d07078
austinnb <austinbrewer@live.com>
2015-09-10 13:55:45 -0400

    [#97564982] Added test app to workspace

---
+++ Ambassador/TestApplication/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  TestApplication
+//
+//  Created by Diplomat on 9/10/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Ambassador/Ambassador.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
