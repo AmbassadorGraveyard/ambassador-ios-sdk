e67eadaf94c85a8fb055442b8232ab8cc125dd31
austinnb <austinbrewer@live.com>
2015-09-15 13:05:39 -0400

    [#103356202] - Changed to static framework project

--- Ambassador/Ambassador/secretObject.h
+++ Ambassador/Ambassador/secretObject.h
@@ -1,14 +1,13 @@
 //
-//  Ambassador.h
+//  secretObject.h
 //  Ambassador
 //
-//  Created by Diplomat on 9/14/15.
+//  Created by Diplomat on 9/15/15.
 //  Copyright (c) 2015 Ambassador. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "TestObject.h"
 
-@interface Ambassador : NSObject
+@interface secretObject : NSObject
 
 @end
---
+++ Ambassador/Ambassador/secretObject.m
@@ -0,0 +1,13 @@
+//
+//  secretObject.m
+//  Ambassador
+//
+//  Created by Diplomat on 9/15/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import "secretObject.h"
+
+@implementation secretObject
+
+@end
