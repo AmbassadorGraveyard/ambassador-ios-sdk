a22114359102caa9d19cd6afbd3d85c82b12a3c4
austinnb <austinbrewer@live.com>
2015-09-16 17:42:36 -0400

    [#103356202] caused duplicate symbol error

---
+++ Ambassador/TestApplication/FMResultSet.m
@@ -0,0 +1,13 @@
+//
+//  FMResultSet.m
+//  Ambassador
+//
+//  Created by Diplomat on 9/16/15.
+//  Copyright (c) 2015 Ambassador. All rights reserved.
+//
+
+#import "FMResultSet.h"
+
+@implementation FMResultSet
+
+@end
