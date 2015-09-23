c1601a700f66ea56237443b157e29e2732c3905e
austinnb <austinbrewer@live.com>
2015-09-23 17:13:23 -0400

    [#100318262] added release framework

---
+++ AmbassadorFramework/Ambassador.framework/Versions/A/Headers/AMBServiceSelectorPreferences.h
@@ -0,0 +1,19 @@
+//
+//  ServiceSelectorPreferences.h
+//  RAF
+//
+//  Created by Diplomat on 7/24/15.
+//  Copyright (c) 2015 ZFERRAL, INC (dba Ambassador Software). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMBServiceSelectorPreferences : NSObject
+
+@property NSString *titleLabelText;
+@property NSString *descriptionLabelText;
+@property NSString *textFieldText;
+@property NSString *navBarTitle;
+@property NSString *defaultShareMessage;
+
+@end
---
+++ AmbassadorFramework/Ambassador.framework/Versions/A/Headers/Ambassador.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+#import "AmbassadorSDK.h"
+#import "AMBConversionParameters.h"
+#import "AMBServiceSelectorPreferences.h"
