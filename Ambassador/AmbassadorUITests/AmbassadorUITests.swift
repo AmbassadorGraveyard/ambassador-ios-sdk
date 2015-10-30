27944e53a6e76f72a2b6b147e29ff817ad26ad7b
jakedunahee <jake@getambassador.com>
2015-10-30 15:35:18 -0400

    [#106128738] Added some fuctioning UI Tests and changed some code to make certain elements accessible

--- Ambassador/AmbassadorUITests/AmbassadorUITests.swift
+++ Ambassador/AmbassadorUITests/AmbassadorUITests.swift
@@ -18,7 +18,15 @@ class AmbassadorUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        let app = XCUIApplication()
+        app.launchArguments = ["USE_MOCK_SERVER"]
+        app.launch()
+        app.buttons["Button"].tap()
+        
+        while (app.otherElements["LoadingView"].exists) {
+            let smallDelay = NSDate().dateByAddingTimeInterval(1)
+            NSRunLoop.mainRunLoop().runUntilDate(smallDelay)
+        }
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -29,37 +37,56 @@ class AmbassadorUITests: XCTestCase {
     }
     
     func testLoadRAF() {
-        
         let app = XCUIApplication()
-        let button = app.buttons["Button"]
-        button.tap()
         
-        let referYourFriendsNavigationBar = app.navigationBars["Refer your friends"]
-        let closeButton = referYourFriendsNavigationBar.buttons["close"]
-        closeButton.tap()
-        button.tap()
+        XCTAssert(app.staticTexts.elementMatchingType(XCUIElementType.StaticText, identifier: "urlLabel").exists)
+        XCTAssert(app.staticTexts["Spread the word"].exists)
+        XCTAssert(app.staticTexts["Refer a friend to get rewards"].exists)
+    
+        XCTAssertEqual(app.collectionViews.cells.count, 5)
         app.buttons["clipboard"].tap()
-        referYourFriendsNavigationBar.tap()
-        button.tap()
-        referYourFriendsNavigationBar.tap()
-        button.tap()
-        closeButton.tap()
-        button.tap()
-        closeButton.tap()
-        button.tap()
-        referYourFriendsNavigationBar.tap()
-        button.tap()
-        referYourFriendsNavigationBar.tap()
-        app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(0).tap()
-        button.tap()
-        closeButton.tap()
-        button.tap()
-        closeButton.tap()
-        button.tap()
-        closeButton.tap()
-        button.tap()
+        XCTAssertEqual(app.staticTexts["lblCopied"].exists, true)
+    }
+    
+    func testCopyButton() {
+        let app = XCUIApplication()
         
+        app.buttons["clipboard"].tap()
+        XCTAssertEqual(app.staticTexts["lblCopied"].exists, true)
         
     }
     
+    func testFacebook() {
+        let app = XCUIApplication()
+        let cell = app.collectionViews.childrenMatchingType(.Cell).elementBoundByIndex(0)
+        cell.tap()
+        
+        let facebookNavigationBar = app.navigationBars["Facebook"]
+        facebookNavigationBar.buttons["Cancel"].tap()
+        
+        cell.tap()
+        facebookNavigationBar.buttons["Post"].tap()
+        
+        XCTAssertEqual(app.buttons["OKAY"].exists, true)
+        app.buttons["OKAY"].tap()
+    }
+    
+    func testTwitter() {
+        let app = XCUIApplication()
+        let cell = app.collectionViews.childrenMatchingType(.Cell).elementBoundByIndex(1)
+        cell.tap()
+        
+        let twitterNavigationBar = app.navigationBars["Twitter"]
+        twitterNavigationBar.buttons["Cancel"].tap()
+        cell.tap()
+        twitterNavigationBar.buttons["Post"].tap()
+        app.buttons["OKAY"].tap()
+        
+    }
+//    
+//    func test() {
+//        XCUIApplication().buttons["Button"].tap()
+//        XCUIApplication().childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element.tap()
+//        
+//    }
 }
