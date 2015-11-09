9de1d83ce5262667ebc5f72dd7b1143ede95ddc1
jakedunahee <jake@getambassador.com>
2015-11-09 14:11:16 -0500

    [#107210544] Add a AMBThemeManager property to test. It sets to the sharedInstance. Added unit tests for colorForKey and messageForKey funtions

--- Ambassador/AmbassadorTests/AMBThemeManagerUnitTests.m
+++ Ambassador/AmbassadorTests/AMBThemeManagerUnitTests.m
@@ -11,12 +11,15 @@
 
 @interface AMBThemeManagerUnitTests : XCTestCase
 
+@property (nonatomic, strong)AMBThemeManager * themeManager;
+
 @end
 
 @implementation AMBThemeManagerUnitTests
 
 - (void)setUp {
     [super setUp];
+    self.themeManager = [AMBThemeManager sharedInstance];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
@@ -38,7 +41,44 @@
 }
 
 - (void)testColorForKey {
+    // GIVEN
+    UIColor *mockNavBarColor = [self colorFromHexString:@"#FFFFFF"];
+    UIColor *mockSendButtonColor = [self colorFromHexString:@"#4199D1"];
+    UIColor *expectedNavBarColor;
+    UIColor *expectedSendButtonColor;
     
+    // WHEN
+    expectedNavBarColor = [self.themeManager colorForKey:NavBarColor];
+    expectedSendButtonColor = [self.themeManager colorForKey:ContactSendButtonBackgroundColor];
+    
+    // THEN
+    XCTAssertEqualObjects(mockNavBarColor, expectedNavBarColor, @"Expected %@ and got %@", mockNavBarColor, expectedNavBarColor);
+    XCTAssertEqualObjects(mockSendButtonColor, expectedSendButtonColor, @"Expected %@ and got %@", mockSendButtonColor, expectedSendButtonColor);
+}
+
+- (void)testMessageForKey {
+    // GIVEN
+    NSString *mockNavBarTitleMessage = @"Refer your friends";
+    NSString *mockWelcomeMessage = @"Spread the word";
+    NSString *expectedNavMessage;
+    NSString *expectedWelcomeMessage;
+    
+    // WHEN
+    expectedNavMessage = [self.themeManager messageForKey:NavBarTextMessage];
+    expectedWelcomeMessage = [self.themeManager messageForKey:RAFWelcomeTextMessage];
+    
+    // THEN
+    XCTAssertTrue([mockNavBarTitleMessage isEqualToString:expectedNavMessage], @"%@ is not equal to %@", mockNavBarTitleMessage, expectedNavMessage);
+    XCTAssertTrue([mockWelcomeMessage isEqualToString:expectedWelcomeMessage], @"%@ is not equal to %@", mockWelcomeMessage, expectedWelcomeMessage);
+}
+
+// Helper function that returns a color from a hex code
+- (UIColor *)colorFromHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
 
 @end
