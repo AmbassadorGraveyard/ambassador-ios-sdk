397c46f01efa2d704eb03aa113df2e3298fd57e3
jakedunahee <jake@getambassador.com>
2015-11-10 18:07:33 -0500

    [#107816636] Finished most unit tests for AmbassadorNetworkManager.  Also rearranged where the nsurl delegate was

--- Ambassador/AmbassadorTests/AMBAmbassadorNetworkManagerTests.m
+++ Ambassador/AmbassadorTests/AMBAmbassadorNetworkManagerTests.m
@@ -55,117 +55,26 @@
     XCTAssertLessThanOrEqual(statusCode, 299);
 }
 
+- (void)testGetPusherChannelName {
+    // GIVEN
+    XCTestExpectation *getPusherExpectation = [self expectationWithDescription:@"Get Pusher name completion handler called"];
+    __block NSError *expectedError;
+    __block NSString *returnString;
+    
+    // WHEN
+    [self.networkManager pusherChannelNameUniversalToken:self.devToken universalID:self.devID completion:^(NSString *string, NSMutableDictionary *dict, NSError *error) {
+        [getPusherExpectation fulfill];
+        expectedError = error;
+        returnString = string;
+    }];
+    
+    [self waitForExpectationsWithTimeout:4 handler:nil];
+    
+    // THEN
+    XCTAssertNil(expectedError, @"There was an error - %@", expectedError);
+    XCTAssertTrue([returnString containsString:@"private-snippet-channel"], @"%@ did not contain private-snippet-channel", returnString);
+}
 
-//- (void)testDevPusherSessionSubscribe {
-//    XCTestExpectation *exp = [self expectationWithDescription:@"Test Dev Pusher Session Subscribe"];
-//    [[AMBAmbassadorNetworkManager sharedInstance] sendNetworkObject:nil url:[AMBAmbassadorNetworkManager pusherSessionSubscribeUrl] universalToken:self.devToken universalID:self.devID additionParams:nil completion:^(NSData *d, NSURLResponse *r, NSError *e) {
-//        if (e) {
-//            XCTFail(@"%@", e);
-//        } else {
-//            NSUInteger code = ((NSHTTPURLResponse *)r).statusCode;
-//            XCTAssert(code >= 200 && code < 300);
-//            
-//            AMBPusherSessionSubscribeNetworkObject* o = [[AMBPusherSessionSubscribeNetworkObject alloc] init];
-//            NSError *err;
-//            [o fillWithDictionary:[NSJSONSerialization JSONObjectWithData:d options:0 error:&err]];
-//            if (err) { XCTFail(@"%@", err); }
-//            
-//            XCTAssertNotNil(o.channel_name);
-//            XCTAssertNotNil(o.expires_at);
-//            XCTAssertNotNil(o.client_session_uid);
-//            
-//            [exp fulfill];
-//        }
-//    }];
-//    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError * __nullable error) {
-//        if (error) {
-//            XCTFail(@"Expectation failed with error: %@", error);
-//        }
-//    }];
-//}
 
-//- (void)testProdPusherSessionSubscribe {
-//    XCTFail();
-//    XCTestExpectation *exp = [self expectationWithDescription:@"Test Prod Pusher Session Subscribe"];
-//    [[AMBAmbassadorNetworkManager sharedInstance] sendNetworkObject:nil url:[AMBAmbassadorNetworkManager pusherSessionSubscribeUrl] universalToken:self.prodToken universalID:self.prodID  additionParams:nil completion:^(NSData *d, NSURLResponse *r, NSError *e) {
-//        if (e) {
-//            XCTFail(@"%@", e);
-//        } else {
-//            NSUInteger code = ((NSHTTPURLResponse *)r).statusCode;
-//            XCTAssert(code >= 200 && code < 300);
-//            
-//            AMBPusherSessionSubscribeNetworkObject* o = [[AMBPusherSessionSubscribeNetworkObject alloc] init];
-//            NSError *err;
-//            [o fillWithDictionary:[NSJSONSerialization JSONObjectWithData:d options:0 error:&err]];
-//            if (err) { XCTFail(@"%@", err); }
-//            
-//            XCTAssertNotNil(o.channel_name);
-//            XCTAssertNotNil(o.expires_at);
-//            XCTAssertNotNil(o.client_session_uid);
-//            NSLog(@"%@\n%@\n%@",o.channel_name, o.expires_at, o.client_session_uid);
-//            
-//            [exp fulfill];
-//        }
-//    }];
-//    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError * __nullable error) {
-//        if (error) {
-//            XCTFail(@"Expectation failed with error: %@", error);
-//        }
-//    }];
-//}
-
-//- (void)testDevSendValidShareTrackObjects {
-//    NSArray *objs = @[
-//                      [AMBMockShareTrackObject validEmailShare],
-//                      [AMBMockShareTrackObject validSMSShare],
-//                      [AMBMockShareTrackObject validLinkedInShare],
-//                      [AMBMockShareTrackObject validTwitterShare],
-//                      [AMBMockShareTrackObject validFacebookShare]
-//                      ];
-//
-//    for (AMBShareTrackNetworkObject *obj in objs) {
-//        [obj toDictionary];
-//        XCTestExpectation *exp = [self expectationWithDescription:@"Test Dev Send Valid Share Track Object"];
-//        [[AMBAmbassadorNetworkManager sharedInstance] sendNetworkObject:obj url:[AMBAmbassadorNetworkManager sendShareTrackUrl] universalToken:self.devToken universalID:self.devID additionParams:nil completion:^(NSData *d, NSURLResponse *r, NSError *e) {
-//            if (e) {
-//                NSLog(@"%@ - %@", obj.social_name, e);
-//                return;
-//            }
-//            [exp fulfill];
-//        }];
-//        [self waitForExpectationsWithTimeout:5.0 handler:^(NSError * __nullable error) {
-//            if (error) {
-//                XCTFail(@"Expectation failed with error: %@", error);
-//            }
-//        }];
-//    }
-//}
-
-//- (void)testDevInvalidShareTrackObjects {
-//    NSArray *objs = @[
-//                      //[AMBMockShareTrackObject invalidRecipientUsernameShare],
-//                      [AMBMockShareTrackObject invalidServiceTypeShare],
-//                      [AMBMockShareTrackObject invalidShortCodeShare],
-//                      //[AMBMockShareTrackObject invalidRecipientEmailShare]
-//                      ];
-//
-//    for (AMBShareTrackNetworkObject *obj in objs) {
-//        [obj toDictionary];
-//        XCTestExpectation *exp = [self expectationWithDescription:@"Test Dev Send Valid Share Track Object"];
-//        [[AMBAmbassadorNetworkManager sharedInstance] sendNetworkObject:obj url:[AMBAmbassadorNetworkManager sendShareTrackUrl] additionParams:nil requestType:@"POST" completion:^(NSData *d, NSURLResponse *r, NSError *e) {
-//            if (!e) {
-//                NSLog(@"%@ didn't throw an error though it should. Here's the data - %@", obj.social_name, [[NSString alloc] initWithData:d encoding:NSASCIIStringEncoding]);
-//
-//                return;
-//            }
-//            [exp fulfill];
-//        }];
-//        [self waitForExpectationsWithTimeout:5.0 handler:^(NSError * __nullable error) {
-//            if (error) {
-//                XCTFail(@"Expectation failed with error: %@", error);
-//            }
-//        }];
-//    }
-//}
 
 @end
