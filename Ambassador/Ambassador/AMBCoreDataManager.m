919f205b6c246b40150f680d61504c393571e761
jakedunahee <jake@getambassador.com>
2015-12-16 11:49:10 -0500

    [#109850466] Added core data test entity

---
+++ Ambassador/Ambassador/AMBCoreDataManager.m
@@ -0,0 +1,13 @@
+//
+//  AMBCoreDataManager.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 12/16/15.
+//  Copyright © 2015 Ambassador. All rights reserved.
+//
+
+#import "AMBCoreDataManager.h"
+
+@implementation AMBCoreDataManager
+
+@end
