8b21fe9289964de2fa80c0f2452ebb163436afd6
jakedunahee <jake@getambassador.com>
2015-12-16 14:19:33 -0500

    [#109850466] Added AMBConversionParameters entity to the coreData model and added dynamic properties to class

--- Ambassador/Ambassador/AMBConversionParameters.m
+++ Ambassador/Ambassador/AMBConversionParameters.m
@@ -18,6 +18,25 @@
 
 @implementation AMBConversionParameters
 
+@dynamic mbsy_campaign;
+@dynamic mbsy_email;
+@dynamic mbsy_first_name;
+@dynamic mbsy_last_name;
+@dynamic mbsy_email_new_ambassador;
+@dynamic mbsy_uid;
+@dynamic mbsy_custom1;
+@dynamic mbsy_custom2;
+@dynamic mbsy_custom3;
+@dynamic mbsy_auto_create;
+@dynamic mbsy_revenue;
+@dynamic mbsy_deactivate_new_ambassador;
+@dynamic mbsy_transaction_uid;
+@dynamic mbsy_add_to_group_id;
+@dynamic mbsy_event_data1;
+@dynamic mbsy_event_data2;
+@dynamic mbsy_event_data3;
+@dynamic mbsy_is_approved;
+
 #pragma mark - Initialization
 - (id)init
 {
