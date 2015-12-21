aaec91039dcd25cefd6a50ab744a440cc3711cbe
jakedunahee <jake@getambassador.com>
2015-12-21 14:31:43 -0500

    [#110325218] Added send identify call to AMBNetworkManager

--- Ambassador/Ambassador/AMBNetworkManager.h
+++ Ambassador/Ambassador/AMBNetworkManager.h
@@ -7,10 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "AMBNetworkObject.h"
 
 @interface AMBNetworkManager : NSObject <NSURLSessionDataDelegate>
 + (instancetype)sharedInstance;
 - (NSMutableURLRequest *)urlRequestFor:(NSString *)url body:(NSData *)b requestType:(NSString*)requestType authorization:(NSString *)a additionalParameters:(NSMutableDictionary*)additParams;
 - (void)dataTaskForRequest:(NSMutableURLRequest *)r session:(NSURLSession *)s completion:( void(^)(NSData *d, NSURLResponse *r, NSError *e))c;
+
+// Network calls
+- (void)sendIdentifyForCampaign:(NSString*)campaign shouldEnroll:(BOOL)enroll success:(void(^)(NSString *response))success failure:(void(^)(NSString *error))failure;
 
 @end
