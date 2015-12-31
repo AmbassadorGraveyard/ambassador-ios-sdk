69c54ab776d0caa32e4888003a1b4bf7974c0794
jakedunahee <jake@getambassador.com>
2015-12-31 17:51:53 -0500

    [#110887026] Added a fullContactClass which holds multiple emails and phonenumbers for a single contact

--- Ambassador/Ambassador/AMBContact.m
+++ Ambassador/Ambassador/AMBContact.m
@@ -8,49 +8,70 @@
 
 #import "AMBContact.h"
 
+
 @implementation AMBContact
 
-#pragma mark - Initialization
-- (id)init
-{
-    if ([super init])
-    {
-        self.firstName = @"";
-        self.lastName = @"";
-        self.label = @"";
-        self.value = @"";
-    }
+
+#pragma mark - Helper functions
+
+- (NSString *)fullName {
+    return [NSString stringWithFormat:@"%@ %@", self.firstName, self.lastName];
+}
+
+@end
+
+
+@implementation AMBFullContact
+
+- (instancetype)initWithABPersonRef:(ABRecordRef)recordRef {
+    NSString *firstName = (__bridge NSString*)ABRecordCopyValue(recordRef, kABPersonFirstNameProperty);
+    firstName = (![firstName isEqualToString:@"(null)"] && firstName) ? firstName : @"";
+    
+    NSString *lastName = (__bridge NSString*)ABRecordCopyValue(recordRef, kABPersonLastNameProperty);
+    lastName = (![lastName isEqualToString:@"(null)"] && lastName) ? lastName : @"";
+    
+    self.phoneContacts = [self getPhoneNumbers:recordRef withFirstName:firstName lastName:lastName];
+    self.emailContacts = [self getEmailAddresses:recordRef withFirstName:firstName lastName:lastName];
     
     return self;
 }
 
-
-
-#pragma mark - Concat Name
-- (NSString *)fullName
-{
-    return [NSString stringWithFormat:@"%@ %@", self.firstName, self.lastName];
+- (NSMutableArray*)getPhoneNumbers:(ABRecordRef)recordRef withFirstName:(NSString*)firstName lastName:(NSString*)lastName {
+    ABMultiValueRef phoneNumbers = ABRecordCopyValue(recordRef, kABPersonPhoneProperty);
+    NSMutableArray *returnArray = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < (int)ABMultiValueGetCount(phoneNumbers); i++) {
+        NSString *phoneLabel = (__bridge NSString*)ABMultiValueCopyLabelAtIndex(phoneNumbers, i);
+        NSString *number = (__bridge NSString*)ABMultiValueCopyValueAtIndex(phoneNumbers, i);
+        
+        AMBContact *phoneContact = [[AMBContact alloc] init];
+        phoneContact.firstName = firstName;
+        phoneContact.lastName = lastName;
+        phoneContact.label = (phoneLabel) ? [phoneLabel stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"_.$!<>"]] : @"Other";
+        phoneContact.value = number;
+        [returnArray addObject:phoneContact];
+    }
+    
+    return returnArray;
 }
 
-
-
-#pragma mark - Overrides for NSSet Storage
-//
-// [NSSet memeber:(id)obj] will test for equality by calling the obj's
-// isEqual: method. To test for equality it compares the hash which is
-// set equal to the value key of the contact which is unique due to NSMutableSet
-// adhearing to strict set requirements. This makes the contacts's value
-// deterministic.
-//
-- (BOOL)isEqual:(id)object
-{
-    AMBContact *obj = (AMBContact *)object;
-    return [self.value isEqualToString:obj.value];
-}
-
-- (NSUInteger)hash
-{
-    return [self.value intValue];
+- (NSMutableArray*)getEmailAddresses:(ABRecordRef)recordRef withFirstName:(NSString*)firstName lastName:(NSString*)lastName  {
+    ABMultiValueRef emailAddresses = ABRecordCopyValue(recordRef, kABPersonEmailProperty);
+    NSMutableArray *returnArray = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < (int)ABMultiValueGetCount(emailAddresses); i++) {
+        NSString *emailLabel = (__bridge NSString*)ABMultiValueCopyLabelAtIndex(emailAddresses, i);
+        NSString *address = (__bridge NSString*)ABMultiValueCopyValueAtIndex(emailAddresses, i);
+        
+        AMBContact *emailContact = [[AMBContact alloc] init];
+        emailContact.firstName = firstName;
+        emailContact.lastName = lastName;
+        emailContact.label = (emailLabel) ? [emailLabel stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"_.$!<>"]] : @"Other";
+        emailContact.value = address;
+        [returnArray addObject:emailContact];
+    }
+    
+    return returnArray;
 }
 
 @end
