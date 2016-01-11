716f737a75a3f349549cd9b7066bd9045047a5d6
jakedunahee <jake@getambassador.com>
2016-01-11 17:54:15 -0500

    [#110887006] Added ReferAFriendViewController class and VC in storyboard

---
+++ Ambassador/TestApplication/ReferAFriendViewController.m
@@ -0,0 +1,27 @@
+//
+//  ReferAFriendViewController.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 1/11/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import "ReferAFriendViewController.h"
+
+@interface ReferAFriendViewController ()
+
+@end
+
+@implementation ReferAFriendViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
