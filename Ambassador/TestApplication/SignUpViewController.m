d3c5022d75e2d6c8106690edb13d7a7853076826
jakedunahee <jake@getambassador.com>
2016-01-11 17:19:02 -0500

    [#110887006] Added signupViewController and set up UI

--- Ambassador/TestApplication/SignUpViewController.m
+++ Ambassador/TestApplication/SignUpViewController.m
@@ -1,29 +1,31 @@
 //
-//  ViewController.m
-//  TestApplication
+//  SignUpViewController.m
+//  Ambassador
 //
-//  Created by Diplomat on 9/14/15.
-//  Copyright (c) 2015 Ambassador. All rights reserved.
+//  Created by Jake Dunahee on 1/11/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "SignUpViewController.h"
 #import "Utilities.h"
-#import <Ambassador/Ambassador.h>
 
-@interface ViewController ()
+@interface SignUpViewController ()
 
 @property (nonatomic, strong) IBOutlet UIView * loginView;
-@property (nonatomic, strong) IBOutlet UIButton * btnLogin;
+@property (nonatomic, strong) IBOutlet UIButton * btnSignUp;
 @property (nonatomic, strong) IBOutlet UIButton * btnDrawer;
+@property (nonatomic, strong) IBOutlet UITextField * tfEmail;
 @property (nonatomic, strong) IBOutlet UITextField * tfUsername;
 @property (nonatomic, strong) IBOutlet UITextField * tfPassword;
 @property (nonatomic, strong) IBOutlet UIView * signInCrossbar;
+@property (nonatomic, strong) IBOutlet UIView * signInCrossbar2;
 
 @end
 
-@implementation ViewController
+@implementation SignUpViewController
 
 - (void)viewDidLoad {
+    [super viewDidLoad];
     [self setUpTheme];
 }
 
@@ -32,15 +34,16 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)setUpTheme {    
+- (void)setUpTheme {
     // Login View
     self.loginView.layer.borderColor = [UIColor colorWithWhite:0.9 alpha:1].CGColor;
     self.signInCrossbar.backgroundColor = [UIColor colorWithWhite:0.9 alpha:1];
+    self.signInCrossbar2.backgroundColor = [UIColor colorWithWhite:0.9 alpha:1];
     self.loginView.layer.borderWidth = 1;
     self.loginView.layer.cornerRadius = 4;
     
     // Login Button
-    self.btnLogin.layer.cornerRadius = 4;
+    self.btnSignUp.layer.cornerRadius = 4;
 }
 
 @end
