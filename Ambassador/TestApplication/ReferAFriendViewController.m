079e807d0fb3c90aeca928479df08de919d11d00
jakedunahee <jake@getambassador.com>
2016-01-12 14:31:51 -0500

    [#110887006] Added delegate functionality to present RAFs onSelection

--- Ambassador/TestApplication/ReferAFriendViewController.m
+++ Ambassador/TestApplication/ReferAFriendViewController.m
@@ -8,6 +8,7 @@
 
 #import "ReferAFriendViewController.h"
 #import "RAFCell.h"
+#import <Ambassador/Ambassador.h>
 
 @interface ReferAFriendViewController () <UITableViewDataSource, UITableViewDelegate>
 
@@ -37,6 +38,29 @@
     [rafCell setUpCellWithRafName:self.rafNameArray[indexPath.row]];
     
     return rafCell;
+}
+
+
+#pragma mark - TableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    switch (indexPath.row) {
+        case 0:
+            [AmbassadorSDK presentRAFForCampaign:@"260" FromViewController:self withThemePlist:nil];
+            break;
+        case 1:
+            [AmbassadorSDK presentRAFForCampaign:@"260" FromViewController:self withThemePlist:nil];
+            break;
+        case 2:
+            [AmbassadorSDK presentRAFForCampaign:@"260" FromViewController:self withThemePlist:nil];
+            break;
+        case 3:
+            [AmbassadorSDK presentRAFForCampaign:@"260" FromViewController:self withThemePlist:nil];
+            break;
+        default:
+            break;
+    }
 }
 
 @end
