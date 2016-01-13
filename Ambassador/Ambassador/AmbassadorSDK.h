43b1a95b619ef6c1fc0f2a141b46ab1882f0b5d1
jakedunahee <jake@getambassador.com>
2016-01-13 18:03:03 -0500

    [#111446226] Cleaned up AmbassadorSDK header a little more

--- Ambassador/Ambassador/AmbassadorSDK.h
+++ Ambassador/Ambassador/AmbassadorSDK.h
@@ -7,16 +7,6 @@
 #import <Foundation/Foundation.h>
 #import "AMBConversionParameters.h"
 
-/**
- `Ambassador` is the object that provides access to Ambassador's SDK functionality
- 
- Subclassing Notes
- This class is provided as an easy way to integrate Ambassador's services in your iOS app and is not intended to be subclassed. Subclassing may lead to unexpected behavior and stability issues.
- 
- Methods to override
- No methods should be overridden. This class is not meant to be subclassed.
- */
-
 @interface AmbassadorSDK : NSObject
 
 /**Initializes Ambassador in an application using the provided credentials*/
@@ -30,6 +20,5 @@
 
 /**Presents a full-page modal 'Refer-A-Friend' (RAF) ViewController*/
 + (void)presentRAFForCampaign:(NSString *)ID FromViewController:(UIViewController *)viewController;
-
 
 @end
