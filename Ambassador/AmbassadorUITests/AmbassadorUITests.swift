e77509e14f2b6136762d820d6d690928c3a1dd30
jakedunahee <jake@getambassador.com>
2016-01-13 11:27:54 -0500

    [#110887006] Added new UI test functionality for TestApplication

--- Ambassador/AmbassadorUITests/AmbassadorUITests.swift
+++ Ambassador/AmbassadorUITests/AmbassadorUITests.swift
@@ -19,8 +19,7 @@ class AmbassadorUITests: XCTestCase {
             app = XCUIApplication()
             app.launchArguments = ["USE_MOCK_SERVER", "isUITesting"]
             app.launch()
-            app.buttons["Button"].tap()
-            
+
             while (app.otherElements["LoadingView"].exists) {
                 let smallDelay = NSDate().dateByAddingTimeInterval(1)
                 NSRunLoop.mainRunLoop().runUntilDate(smallDelay)
@@ -33,6 +32,64 @@ class AmbassadorUITests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func identifyWithLogin() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["Login"].tap()
+        
+        let usernameTextField = app.textFields["Username"]
+        usernameTextField.tap()
+        usernameTextField.typeText("jake@getambassador.com")
+        XCTAssertEqual(app.keyboards.count, 1) // Checks to make sure keyboard is present
+        
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("testpassword")
+        XCTAssertEqual(app.keyboards.count, 1) // Checks to make sure keyboard is present
+        
+        app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.buttons["Login"].tap()
+        XCTAssertEqual(app.keyboards.count, 0) // Checks to sure all textFields resigned firstResponder (that the keyboard is hidden)
+    }
+    
+    func testSignUpInstallConversion() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["Sign Up"].tap()
+        
+        let emailTextField = app.textFields["Email"]
+        emailTextField.tap()
+        emailTextField.typeText("jake@getambassador.com")
+        XCTAssertEqual(app.keyboards.count, 1) // Checks to make sure keyboard is present
+        
+        let usernameTextField = app.textFields["Username"]
+        usernameTextField.tap()
+        usernameTextField.typeText("testuser")
+        XCTAssertEqual(app.keyboards.count, 1) // Checks to make sure keyboard is present
+        
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("testpassword")
+        XCTAssertEqual(app.keyboards.count, 1) // Checks to make sure keyboard is present
+        
+        app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.buttons["Sign Up"].tap()
+        XCTAssertEqual(app.keyboards.count, 0) // Checks to sure all textFields resigned firstResponder (that the keyboard is hidden)
+    }
+    
+    func testBuyConversion() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["Buy Now"].tap()
+        app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.buttons["Buy Now"].tap()
+        
+        let doneButton = app.alerts["Purchase successful"].collectionViews.buttons["Done"]
+        doneButton.tap()
+        XCTAssertFalse(doneButton.exists)
+    }
+    
+    func testPresentRAF() {
+        self.identifyWithLogin()
+        let app = XCUIApplication()
+        app.tabBars.buttons["Refer a Friend"].tap()
+        app.tables.staticTexts["Shopping Cart RAF"].tap()
     }
     
     func testLoadRAF() {
