d76ab03cc605b2022cfe6a81114e2ee71536e4f2
jakedunahee <jake@getambassador.com>
2016-01-13 17:58:01 -0500

    [#111446226] Updated documentation that a user sees when calling AmbassadorSDK functions

--- Ambassador/Ambassador/AmbassadorSDK.h
+++ Ambassador/Ambassador/AmbassadorSDK.h
@@ -18,62 +18,17 @@
  */
 
 @interface AmbassadorSDK : NSObject
-///--------------------
-/// @name Initial Setup
-///--------------------
 
-/**
- Runs an 'Ambassador' object with the specified API key.
- 
- @param universalToken The Universal Token provided to you by Ambassador.
- @param universalID The Universal ID provided to you by Ambassador.
- */
+/**Initializes Ambassador in an application using the provided credentials*/
 + (void)runWithUniversalToken:(NSString *)UniversalToken universalID:(NSString *)universalID;
 
-/**
- Runs an 'Ambassador' object with the specified API key and parameters for registering a conversion on launch
- 
- @param universalToken The Universal Token provided to you by Ambassador.
- @param universalID The Universal ID provided to you by Ambassador.
- @param parameters An instance of ConversionParameters with properties set for registering a conversion on the initial launch of the app.
- @param a completion handler called upon registering the conversion
- Error domain = AmbassadorSDKErrorDomain
- Error codes :
- 1   ConversionParameters required properties unset
- 2   ConversionParameters property has nil value
- */
-
-
-///------------------------------------
-/// @name Core Ambassador Functionality
-///------------------------------------
-
-/**
- Identifies a user and associates it with the given email
- 
- @param email The user's email to be used in identifying the Ambassador
- */
+/**Identifies a user and associates it with the given email*/
 + (void)identifyWithEmail:(NSString *)email;
 
-/**
- Registers a conversion with Ambassador per the properties set in 'parameters'
- 
- @param parameters An instance of ConversionParameters with properties set for registering a conversion on the initial launch of the app.
- @param a completion handler called upon registering the conversion. 
- Error domain = AmbassadorSDKErrorDomain
- Error codes :
-                1   ConversionParameters required properties unset 
-                2   ConversionParameters property has nil value
- */
+/**Registers a conversion with Ambassador using the properties set in 'conversionParameters'*/
 + (void)registerConversion:(AMBConversionParameters *)conversionParameters restrictToInstall:(BOOL)restrictToInstall completion:(void (^)(NSError *error))completion;
 
-/**
- Presents a full-page modal 'Refer-A-Friend' (RAF) view controller
- 
- @param ID The Ambassador campaign id with which you would like this RAF associated.
- @param viewController The view controller from which you would like to present the RAF.
- @param parameters An instance of ServiceSelectorpreferences with properties set to customize text properties of the RAF
- */
+/**Presents a full-page modal 'Refer-A-Friend' (RAF) ViewController*/
 + (void)presentRAFForCampaign:(NSString *)ID FromViewController:(UIViewController *)viewController;
 
 
