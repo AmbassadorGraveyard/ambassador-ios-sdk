c34b91fb45ce4b7bea67e78d2f90b8fa2625f8a7
jakedunahee <jake@getambassador.com>
2016-01-14 15:36:40 -0500

    [#111449556] Refactored LinkedIn authorization webview code

--- Ambassador/Ambassador/AMBAuthorizeLinkedIn.m
+++ Ambassador/Ambassador/AMBAuthorizeLinkedIn.m
@@ -10,11 +10,12 @@
 #import "AMBConstants.h"
 #import "AMBNetworkManager.h"
 
-@interface AMBAuthorizeLinkedIn () <UIWebViewDelegate>
+@interface AMBAuthorizeLinkedIn () <UIWebViewDelegate, UIAlertViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
+
 
 @implementation AMBAuthorizeLinkedIn
 
@@ -34,42 +35,20 @@
 }
 
 
-#pragma mark - WebViewDelegate
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
-{
+#pragma mark - WebView Delegate
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     //Parse the URL string delimiting at "?"
     NSString *urlRequestString = [[request URL] absoluteString];
     NSArray *urlRequestComponents = [urlRequestString componentsSeparatedByString:@"?"];
-    if ([[urlRequestComponents firstObject] isEqualToString:[AMBValues getLinkedInAuthCallbackUrl]])
-    {
-        self.webView.hidden = YES;
-        if (urlRequestComponents.count > 1)
-        {
-            NSArray *queryParameters = [urlRequestComponents[1] componentsSeparatedByString:@"&"];
-            for (int i = 0; i < queryParameters.count; ++i)
-            {
-                NSArray *queryPair = [queryParameters[i] componentsSeparatedByString:@"="];
-                if ([[queryPair firstObject] isEqualToString:@"error"]) {
-                    [self.navigationController popViewControllerAnimated:YES];
-                }
-                
-                if ([[queryPair firstObject] isEqualToString:@"code"]) {
-                    [[AMBNetworkManager sharedInstance] getLinkedInRequestTokenWithKey:[NSString stringWithString:[queryPair lastObject]] success:^{
-                        DLog(@"Get Linkedin Request Token SUCCESSFUL!")
-                        [self.delegate userDidContinue];
-                    } failure:^(NSString *error) {
-                        DLog(@"Get Linkedin Request Token FAILED with response - %@", error)
-                    }];
-                }
-            }
-        }
-        return NO;
+    
+    if ([[urlRequestComponents firstObject] isEqualToString:[AMBValues getLinkedInAuthCallbackUrl]] && urlRequestComponents.count > 1) { // Checks if the webview is redirecting to the callback url and that there are parameters
+        NSArray *queryParameters = [urlRequestComponents[1] componentsSeparatedByString:@"&"]; // Creates an array of query parameters and corresponding values Ex:test=value, test2=value2
+        [self saveValuesFromQueryParams:queryParameters];
     }
+    
     return YES;
 }
-
-
-#pragma mark - WebView Delegate
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
     if (!webView.isLoading) {
@@ -77,5 +56,35 @@
     }
 }
 
+
+#pragma mark - UIAlertView
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+
+#pragma mark - Helper Functions
+
+- (void)saveValuesFromQueryParams:(NSArray*)queryParameters {
+    for (int i = 0; i < queryParameters.count; ++i) {
+        NSArray *queryPair = [queryParameters[i] componentsSeparatedByString:@"="];
+        
+        if ([[queryPair firstObject] isEqualToString:@"error"]) { // If a random error occurs, the user is alerted and popped back to try again
+            UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Unable to login" message:@"There was an error while logging into LinkedIn, please try again" delegate:self cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+            [errorAlert show];
+            return;
+        }
+    
+        if ([[queryPair firstObject] isEqualToString:@"code"]) {
+            [[AMBNetworkManager sharedInstance] getLinkedInRequestTokenWithKey:[NSString stringWithString:[queryPair lastObject]] success:^{
+                DLog(@"Get Linkedin Request Token SUCCESSFUL!")
+                [self.delegate userDidContinue];
+            } failure:^(NSString *error) {
+                DLog(@"Get Linkedin Request Token FAILED with response - %@", error)
+            }];
+        }
+    }
+}
 
 @end
