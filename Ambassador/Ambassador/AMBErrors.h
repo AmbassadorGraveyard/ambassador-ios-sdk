84ba47ed5770c35abe268d9760a6ced4ae2e7340
jakedunahee <jake@getambassador.com>
2016-01-15 16:57:18 -0500

    [#111450002] Cleared out old AMBErrors code and added conversionError function

--- Ambassador/Ambassador/AMBErrors.h
+++ Ambassador/Ambassador/AMBErrors.h
@@ -6,30 +6,12 @@
 //  Copyright © 2015 Ambassador. All rights reserved.
 //
 
-#ifndef AMBErrors_h
-#define AMBErrors_h
-
-#define AMBErrorDomain      "AmbassadorErrorDomain"
-#define AMBINVNETOBJ        1   /* Invalid Network Object */
-#define AMBBADRESP          2   /* Non-200 Server Response */
-#define AMBCDINVMOC         3   /* Core Data Error */
-#define AMBNOPERMISS        4   /* Lacking Permission to Access Data */
-#define AMBINVSMSNUM        5   /* Invalid SMS Number */
-#define AMBNOIDENT          6   /* No Identify Error */
-#define AMBINVLKDNAUTH      7   /* LinkedIn Authentication Error */
-#define AMBINVPARAM         8   /* Invalid Method Parameters */
-#define AMBNOVAL            9   /* No Value Return */
-
 #import <Foundation/Foundation.h>
 
-NSError* AMBINVETOBJError(id sender);
-NSError* AMBBADRESPError(NSUInteger code, NSData *data);
-NSError* AMBCDINVMOCError();
-NSError* AMBNOPERMISSError(id sender);
-NSError* AMBINVSMSNUMError(NSMutableArray *badVals);
-NSError* AMBNOIDENTError();
-NSError* AMBINVLKDNAUTHError(NSString *msg);
-NSError* AMBINVPARAMError(NSString *methodName);
-NSError* AMBNOVALError();
+@interface AMBErrors : NSObject
 
-#endif /* AMBErrors_h */
++ (void)conversionError:(NSInteger)statusCode errorData:(NSData*)data;
+
+@end
+
+
