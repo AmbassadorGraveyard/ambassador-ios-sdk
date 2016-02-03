a50b1435e743f614bd7b4f7af3ad15c76e392c85
jakedunahee <jake@getambassador.com>
2016-02-03 17:01:34 -0500

    [#112351365] Started pusher tests

--- Ambassador/AmbassadorTests/AMBPusherManagerTests.m
+++ Ambassador/AmbassadorTests/AMBPusherManagerTests.m
@@ -7,22 +7,73 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
 #import "AMBPusherManager.h"
 #import "AMBTests.h"
+#import "AMBPTPusher.h"
 
-@interface AMBPusherManagerTests : AMBTests
-@property NSString *channelName;
+@interface AMBPusherManager (Test)
+
+@property (nonatomic, strong) NSString *universalToken;
+@property (nonatomic, strong) AMBPTPusher *client;
+
+- (instancetype)initWithAuthorization:(NSString *)auth;
+
+@end
+
+
+@interface AMBPusherManagerTests : XCTestCase
+
+@property (nonatomic, strong) AMBPusherManager * pusherMgr;
+
 @end
 
 @implementation AMBPusherManagerTests
 
 - (void)setUp {
     [super setUp];
-    self.channelName = @"test_channel";
+    if (!self.pusherMgr) {
+        self.pusherMgr = [AMBPusherManager sharedInstanceWithAuthorization:@"testAuth"];
+    }
 }
 
 - (void)tearDown {
     [super tearDown];
+}
+
+
+#pragma mark - LifeCycle Tests
+
+- (void)testSharedInstance {
+    XCTAssertNotNil(self.pusherMgr);
+}
+
+
+#pragma mark - Initialization Tests
+
+- (void)testInitWithAuth {
+    // GIVEN
+    NSString *fakeToken = @"fakeToken123";
+    
+    // WHEN
+    AMBPusherManager *mockMgr = [self.pusherMgr initWithAuthorization:fakeToken];
+    
+    // THEN
+    XCTAssertEqualObjects(fakeToken, mockMgr.universalToken);
+}
+
+- (void)testSubscribeToChannel {
+    // GIVEN
+    NSString *channelName = @"fakeChannelName";
+    id mockPusher = [OCMockObject mockForClass:[AMBPTPusher class]];
+    self.pusherMgr.client = mockPusher;
+    [[[mockPusher expect] andDo:nil] subscribeToPrivateChannelNamed:channelName];
+    
+    // WHEN
+    [self.pusherMgr subscribeToChannel:channelName completion:nil];
+    
+    // THEN
+    [mockPusher verify];
 }
 
 @end
