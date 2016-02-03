29220b6edb78cce4835805878147696768fb5763
jakedunahee <jake@getambassador.com>
2016-02-03 13:42:04 -0500

    [#112351319] Finished identify tests

--- Ambassador/AmbassadorTests/AMBIdenityUnitTests.m
+++ Ambassador/AmbassadorTests/AMBIdenityUnitTests.m
@@ -7,16 +7,36 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+#import <SafariServices/SafariServices.h>
+#import "AMBIdentify.h"
+#import "AMBValues.h"
+
+@interface AMBIdentify (Tests)
+
+@property (nonatomic, strong) NSTimer * identifyTimer;
+@property (nonatomic, strong) SFSafariViewController * safariVC;
+
+- (void)deviceInfoReceived;
+- (void)performIdentifyForiOS9;
+
+@end
+
 
 @interface AMBIdenityUnitTests : XCTestCase
 
+@property (nonatomic, strong) AMBIdentify * identify;
+
 @end
+
 
 @implementation AMBIdenityUnitTests
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    if (!self.identify) {
+        self.identify = [[AMBIdentify alloc] init];
+    }
 }
 
 - (void)tearDown {
@@ -24,16 +44,46 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testGetIdentify {
+    // GIVEN
+    id mockIdentify = [OCMockObject partialMockForObject:self.identify];
+    
+    if ([[NSProcessInfo processInfo] operatingSystemVersion].majorVersion >= 9.0) {
+        [[[mockIdentify expect] andDo:nil] performIdentifyForiOS9];
+    }
+    
+    // WHEN
+    [self.identify getIdentity];
+    
+    // THEN
+    [mockIdentify verify];
+    XCTAssertNotNil(self.identify.identifyTimer);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testPerformIdentifyForiOS9 {
+    // WHEN
+    [self.identify performIdentifyForiOS9];
+    
+    // THEN
+    XCTAssertNotNil(self.identify.safariVC);
+    XCTAssertTrue([self.identify.safariVC.view isHidden]);
+}
+
+- (void)testDeviceInfoReceived {
+    // GIVEN
+    self.identify.safariVC = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:@"https://google.com"]];
+    id mockSafariVC = [OCMockObject partialMockForObject:self.identify.safariVC];
+    id mockView = [OCMockObject partialMockForObject:self.identify.safariVC.view];
+
+    [[[mockSafariVC expect] andDo:nil] removeFromParentViewController];
+    [[[mockView expect] andDo:nil] removeFromSuperview];
+    
+    // WHEN
+    [self.identify deviceInfoReceived];
+    
+    // THEN
+    [mockSafariVC verify];
+    [mockView verify];
 }
 
 @end
