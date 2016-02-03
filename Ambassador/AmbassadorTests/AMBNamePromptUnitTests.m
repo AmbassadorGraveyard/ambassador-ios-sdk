2f12a1f76b42ca1daf142e68db728702bf4ceaba
jakedunahee <jake@getambassador.com>
2016-02-03 15:19:38 -0500

    [#112349387] Added navigation and textfield delegate tests

--- Ambassador/AmbassadorTests/AMBNamePromptUnitTests.m
+++ Ambassador/AmbassadorTests/AMBNamePromptUnitTests.m
@@ -10,11 +10,18 @@
 #import <OCMock/OCMock.h>
 #import "AMBNamePrompt.h"
 
-@interface AMBNamePrompt (Test)
+@interface AMBNamePrompt (Test) <UITextFieldDelegate>
 
 @property (nonatomic, strong) IBOutlet NSLayoutConstraint * topConstraint;
+@property (nonatomic, strong) IBOutlet UITextField *firstNameField;
+@property (nonatomic, strong) IBOutlet UITextField *lastNameField;
 
 - (void)setUpTheme;
+- (void)checkForBlankNames:(NSString*)firstName lastName:(NSString*)lastName;
+- (IBAction)btnCloseTapped:(id)sender;
+- (IBAction)continueSending:(UIButton *)sender;
+- (void)backButtonPressed:(UIButton *)button;
+- (void)removeErrors;
 
 @end
 
@@ -77,6 +84,82 @@
     
     // THEN
     [mockObserver verify];
+}
+
+
+#pragma mark - IBActions Tests
+
+- (void)testBtnCloseTapped {
+    // GIVEN
+    [[[self.mockNamePrompt expect] andDo:nil] dismissViewControllerAnimated:YES completion:nil];
+    
+    // WHEN
+    [self.namePrompt btnCloseTapped:nil];
+    
+    // THEN
+    [self.mockNamePrompt verify];
+}
+
+- (void)testContinueSending {
+    // GIVEN
+    NSString *firstName = @"FirstName";
+    NSString *lastName = @"LastName";
+    
+    self.namePrompt.firstNameField = [[UITextField alloc] init];
+    self.namePrompt.lastNameField = [[UITextField alloc] init];
+    self.namePrompt.firstNameField.text = firstName;
+    self.namePrompt.lastNameField.text = lastName;
+    
+    [[[self.mockNamePrompt expect] andDo:nil] checkForBlankNames:firstName lastName:lastName];
+    
+    // WHEN
+    [self.namePrompt continueSending:nil];
+    
+    // THEN
+    [self.mockNamePrompt verify];
+}
+
+
+#pragma mark - Navigation Tests
+
+- (void)testBackButtonPressed {
+    // GIVEN
+    id mockNav = [OCMockObject mockForClass:[UINavigationController class]];
+    [[[self.mockNamePrompt expect] andReturn:mockNav] navigationController];
+    [[[mockNav expect] andDo:nil] popViewControllerAnimated:YES];
+    
+    // WHEN
+    [self.namePrompt backButtonPressed:nil];
+    
+    // THEN
+    [mockNav verify];
+}
+
+
+#pragma mark - TextField Delegate Tests
+
+- (void)testTextFieldShouldChangeCharactersTest {
+    // GIVEN
+    [[[self.mockNamePrompt expect] andDo:nil] removeErrors];
+    
+    // WHEN
+    [self.namePrompt textField:self.namePrompt.firstNameField shouldChangeCharactersInRange:NSRangeFromString(@"test") replacementString:@"test"];
+    
+    // THEN
+    [self.mockNamePrompt verify];
+}
+
+- (void)testTextFieldShouldReturn {
+    // GIVEN
+    self.namePrompt.firstNameField = [[UITextField alloc] init];
+    id mockTF = [OCMockObject partialMockForObject:self.namePrompt.firstNameField];
+    [[[mockTF expect] andDo:nil] resignFirstResponder];
+    
+    // WHEN
+    [self.namePrompt textFieldShouldReturn:mockTF];
+    
+    // THEN
+    [mockTF resignFirstResponder];
 }
 
 @end
