bc1fb371598432efa1eed846791c5f967a989813
jakedunahee <jake@getambassador.com>
2016-02-08 18:10:47 -0500

    [#112349105]  Working on contactCell unit tests

--- Ambassador/AmbassadorTests/AMBContactCellUnitTests.m
+++ Ambassador/AmbassadorTests/AMBContactCellUnitTests.m
@@ -7,8 +7,24 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+#import "AMBContactCell.h"
+
+@interface AMBContactCell (Test)
+
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint * checkmarkConstraint;
+@property (nonatomic, weak) IBOutlet UIImageView * checkmarkView;
+
+- (void)setCellSelectionColor;
+- (void)longPressTriggered:(UILongPressGestureRecognizer*)sender;
+
+@end
+
 
 @interface AMBContactCellUnitTests : XCTestCase
+
+@property (nonatomic, strong) AMBContactCell * contactCell;
+@property (nonatomic) id mockCell;
 
 @end
 
@@ -16,24 +32,99 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    if (!self.contactCell) {
+        self.contactCell = [[AMBContactCell alloc] init];
+    }
+    
+    self.mockCell = [OCMockObject partialMockForObject:self.contactCell];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [self.mockCell stopMocking];
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testSetUpCell {
+    // GIVEN
+    
+    // WHEN
+    
+    // THEN
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+
+#pragma mark - Helper Function Tests
+
+//- (void)testLongPressTriggered {
+//    // GIVEN
+//    id mockGesture = [OCMockObject mockForClass:[UILongPressGestureRecognizer class]];
+//    [[[mockGesture expect] andReturnValue:OCMOCK_VALUE(UIGestureRecognizerStateBegan)] state];
+//    
+//    id mockDelegate = [OCMockObject mockForProtocol:@protocol(AMBContactCellDelegate)];
+//    [[[mockDelegate expect] andDo:nil] longPressTriggeredForContact:[OCMArg any]];
+//    
+//    // WHEN
+//    [self.contactCell longPressTriggered:mockGesture];
+//    
+//    // THEN
+//    [mockGesture verify];
+//    [mockDelegate verify];
+//}
+
+
+#pragma mark - UI Functions Tests
+
+- (void)testAnimateCheckMarkIn {
+    // GIVEN
+    id mockConstraint = [OCMockObject mockForClass:[NSLayoutConstraint class]];
+    [[[mockConstraint expect] andDo:nil] setConstant:16];
+    [[[mockConstraint expect] andReturnValue:OCMOCK_VALUE(16)] constant];
+    
+    self.contactCell.checkmarkConstraint = mockConstraint;
+    NSInteger expectedConstraintSize = 16;
+    
+    // WHEN
+    [self.contactCell animateCheckmarkIn];
+    
+    // THEN
+    XCTAssertEqual(expectedConstraintSize, self.contactCell.checkmarkConstraint.constant);
+    [mockConstraint stopMocking];
+}
+
+- (void)testAnimateCheckMarkOut {
+    // GIVEN
+    id mockConstraint = [OCMockObject mockForClass:[NSLayoutConstraint class]];
+    [[[mockConstraint expect] andDo:nil] setConstant:-50];
+    [[[mockConstraint expect] andReturnValue:OCMOCK_VALUE(-50)] constant];
+    
+    id mockCheckmark = [OCMockObject mockForClass:[UIImageView class]];
+    [[[mockCheckmark expect] andReturnValue:OCMOCK_VALUE(CGRectMake(0, 0, 50, 0))] frame];
+    self.contactCell.checkmarkView = mockCheckmark;
+    
+    self.contactCell.checkmarkConstraint = mockConstraint;
+    NSInteger expectedConstraintSize = -50;
+    
+    // WHEN
+    [self.contactCell animateCheckmarkOut];
+    
+    // THEN
+    XCTAssertEqual(expectedConstraintSize, self.contactCell.checkmarkConstraint.constant);
+    [mockConstraint stopMocking];
+}
+
+- (void)testSetCellSelectionColor {
+    // GIVEN
+    id mockView = [OCMockObject mockForClass:[UIView class]];
+    [[[mockView expect] andReturnValue:mockView] alloc];
+    [[[mockView expect] andDo:nil] setBackgroundColor:[OCMArg any]];
+    
+    // WHEN
+    [self.contactCell setCellSelectionColor];
+    
+    
+    // THEN
+    [mockView verify];
+    XCTAssertEqual(self.contactCell.selectedBackgroundView, mockView);
 }
 
 @end
