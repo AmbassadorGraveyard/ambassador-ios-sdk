7c3341386475f30021ca5ab49f8c563147afbf15
jakedunahee <jake@getambassador.com>
2016-02-29 16:59:08 -0500

    [#114124191] Added welcomeScreenViewController class

---
+++ Ambassador/Ambassador/AMBWelcomeScreenViewController.m
@@ -0,0 +1,13 @@
+//
+//  AMBWelcomeScreenViewController.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 2/29/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import "AMBWelcomeScreenViewController.h"
+
+@implementation AMBWelcomeScreenViewController
+
+@end
