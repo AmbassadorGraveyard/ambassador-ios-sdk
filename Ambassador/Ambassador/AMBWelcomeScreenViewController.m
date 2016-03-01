f05c8ce9c4f469b25ece8fd0f669ab40b632a814
jakedunahee <jake@getambassador.com>
2016-03-01 11:37:40 -0500

    [#114124191] Finished setting up UI

--- Ambassador/Ambassador/AMBWelcomeScreenViewController.m
+++ Ambassador/Ambassador/AMBWelcomeScreenViewController.m
@@ -7,21 +7,28 @@
 //
 
 #import "AMBWelcomeScreenViewController.h"
+#import "AMBLinkCell.h"
 
-@interface AMBWelcomeScreenViewController()
+@interface AMBWelcomeScreenViewController() <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
 
 @property (nonatomic, strong) IBOutlet UIView * masterView;
 @property (nonatomic, strong) IBOutlet UIImageView * ivProfilePic;
 @property (nonatomic, strong) IBOutlet UILabel * lblReferred;
 @property (nonatomic, strong) IBOutlet UILabel * lblDescription;
 @property (nonatomic, strong) IBOutlet UIButton * btnAction;
+@property (nonatomic, strong) IBOutlet UICollectionView * linkCollectionView;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint * collectionViewHeight;
 @property (nonatomic, strong) IBOutlet NSLayoutConstraint * buttonHeight;
 @property (nonatomic, strong) IBOutlet NSLayoutConstraint * masterViewTop;
+
+@property (nonatomic, strong) NSArray * linkArray;
 
 @end
 
 
 @implementation AMBWelcomeScreenViewController
+
+NSInteger const CELL_HEIGHT = 25;
 
 
 #pragma mark - LifeCycle
@@ -29,17 +36,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setTheme];
-    NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://www.dogbehaviourltd.co.uk/userimages/Bulldog_Puppy_Posed_On_White_Background.jpg"]];
+    NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://fraziercreative.agency/wp-content/uploads/2013/07/cool-guy.jpg"]];
     self.ivProfilePic.image = [UIImage imageWithData:imageData];
     self.masterViewTop.constant = -(self.masterViewTop.constant + self.masterView.frame.size.height);
+    
+    self.linkArray = @[@"Link Two", @"Link One"];
+    [self setupCollectionView];
 }
 
 - (void)viewDidLayoutSubviews {
     if (self.masterViewTop.constant < 30) {
         self.masterViewTop.constant = 30;
-        [UIView animateKeyframesWithDuration:0.6 delay:0 options:UIViewKeyframeAnimationOptionCalculationModeCubic animations:^{
-            [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.5 animations:^{
+        [UIView animateKeyframesWithDuration:0.5 delay:0 options:UIViewKeyframeAnimationOptionCalculationModeCubic animations:^{
+            [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.4 animations:^{
                 [self.view layoutIfNeeded];
+                self.ivProfilePic.layer.cornerRadius = self.ivProfilePic.frame.size.height/2;
             }];
         } completion:nil];
     }
@@ -53,6 +64,24 @@
 }
 
 
+#pragma mark - CollectionView Delegate
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [self.linkArray count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    AMBLinkCell *linkCell = [collectionView dequeueReusableCellWithReuseIdentifier:@"linkCell" forIndexPath:indexPath];
+    [linkCell setupCellWithLinkName:self.linkArray[indexPath.row] tintColor:self.btnAction.backgroundColor];
+    return linkCell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CGSize cellSize = [self.linkArray count] > 2 ? CGSizeMake(collectionView.frame.size.width, CELL_HEIGHT) : CGSizeMake(collectionView.frame.size.width/2, collectionView.frame.size.height);
+    return cellSize;
+}
+
+
 #pragma mark - UI Functions
 
 - (void)setTheme {
@@ -63,6 +92,14 @@
     self.ivProfilePic.layer.cornerRadius = self.ivProfilePic.frame.size.height/2;
     self.ivProfilePic.layer.borderColor = self.btnAction.backgroundColor.CGColor;
     self.ivProfilePic.layer.borderWidth = 2;
+}
+
+- (void)setupCollectionView {
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.scrollDirection = [self.linkArray count] > 2 ? UICollectionViewScrollDirectionVertical : UICollectionViewScrollDirectionHorizontal;
+    [self.linkCollectionView setCollectionViewLayout:layout];
+    self.linkCollectionView.scrollEnabled = [self.linkArray count] > 2 ? YES : NO;
+    if ([self.linkArray count] > 2) { self.collectionViewHeight.constant = 100; }
 }
 
 @end
