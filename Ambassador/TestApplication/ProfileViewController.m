72b619c63f24a43d891bbd65614d98c8a02a3c6c
jakedunahee <jake@getambassador.com>
2016-03-03 17:20:52 -0500

    [#114124159] Added Profile section for the ambassador logged in

---
+++ Ambassador/TestApplication/ProfileViewController.m
@@ -0,0 +1,48 @@
+//
+//  ProfileViewController.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/3/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+#import "DefaultsHandler.h"
+
+@interface ProfileViewController()
+
+@property (nonatomic, strong) IBOutlet UIImageView * ivAvatar;
+@property (nonatomic, strong) IBOutlet UILabel * lblFullName;
+@property (nonatomic, strong) IBOutlet UIButton * btnSignOut;
+@property (nonatomic, strong) IBOutlet UIView * customNav;
+
+@end
+
+
+@implementation ProfileViewController
+
+
+#pragma mark - LifeCycle
+
+- (void)viewDidLoad {
+    [self setupUI];
+}
+
+
+#pragma mark - UI Functions
+
+- (void)setupUI {
+    // Avatar
+    self.ivAvatar.image = [DefaultsHandler getUserImage];
+    self.ivAvatar.layer.cornerRadius = self.ivAvatar.frame.size.height/2;
+    self.ivAvatar.layer.borderColor = self.customNav.backgroundColor.CGColor;
+    self.ivAvatar.layer.borderWidth = 2;
+    
+    // Label
+    self.lblFullName.text = [DefaultsHandler getFullName];
+    
+    // Button
+    self.btnSignOut.layer.cornerRadius = 6;
+}
+
+@end
