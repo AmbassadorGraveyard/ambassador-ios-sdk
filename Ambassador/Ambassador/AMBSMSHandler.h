0a3c79ae6000f0cd45996d1c8a576a1f54f50ca8
jakedunahee <jake@getambassador.com>
2016-03-10 10:34:54 -0500

    [#115151223] Added new SMSHandler class

---
+++ Ambassador/Ambassador/AMBSMSHandler.h
@@ -0,0 +1,13 @@
+//
+//  AMBSMSHandler.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/10/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMBSMSHandler : NSObject
+
+@end
