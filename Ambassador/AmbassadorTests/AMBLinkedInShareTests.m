dc89146ae09cc2e8937365b675be9546300e9392
jakedunahee <jake@getambassador.com>
2016-03-11 11:50:02 -0500

    [#115151223] Removed broken test

--- Ambassador/AmbassadorTests/AMBLinkedInShareTests.m
+++ Ambassador/AmbassadorTests/AMBLinkedInShareTests.m
@@ -44,20 +44,6 @@
 
 #pragma mark - LifeCycle Tests
 
-- (void)testViewDidLoad {
-    // GIVEN
-    NSString *titleString = @"LinkedIn";
-    NSString *fakeDefaultMessage = @"Share stuff!";
-    self.linkedInShareVC.defaultMessage = fakeDefaultMessage;
-    
-    // WHEN
-    [self.linkedInShareVC viewDidLoad];
-    
-    // THEN
-    XCTAssertEqualObjects(titleString, self.linkedInShareVC.title);
-    XCTAssertEqualObjects(fakeDefaultMessage, self.linkedInShareVC.textView.text);
-}
-
 - (void)testViewDidAppear {
     // GIVEN
     id mockTextView = [OCMockObject mockForClass:[UITextView class]];
