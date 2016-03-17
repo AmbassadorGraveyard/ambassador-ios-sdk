b570a10d30e8be0773203196b75a7c6dd46806ef
jakedunahee <jake@getambassador.com>
2016-03-17 14:32:50 -0400

    [#115071307] Moved Ambassador Login to the tabBarController

--- Ambassador/TestApplication/MyTabBarController.m
+++ Ambassador/TestApplication/MyTabBarController.m
@@ -7,12 +7,38 @@
 //
 
 #import "MyTabBarController.h"
+#import "DefaultsHandler.h"
+#import <Ambassador/Ambassador.h>
+
+@interface MyTabBarController()
+
+@property (nonatomic) BOOL hasPerformedRunWithKeys;
+
+@end
+
 
 @implementation MyTabBarController
 
-- (void)viewDidAppear:(BOOL)animated {
+NSString * loginSegue = @"ambassador_login_segue";
+
+- (void)viewDidLoad {
     self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:0.23 green:0.59 blue:0.83 alpha:1];
     self.navigationController.navigationBar.titleTextAttributes = @{ NSForegroundColorAttributeName:[UIColor whiteColor]};
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    if (![self hasPerformedRunWithKeys]) {
+        [self checkForLogin];
+    }
+}
+
+- (void)checkForLogin {
+    if ([[DefaultsHandler getSDKToken] isEqualToString:@""] || [[DefaultsHandler getUniversalID] isEqualToString:@""]) {
+        [self performSegueWithIdentifier:loginSegue sender:self];
+    } else {
+        self.hasPerformedRunWithKeys = YES;
+        [AmbassadorSDK runWithUniversalToken:[DefaultsHandler getSDKToken] universalID:[DefaultsHandler getUniversalID]];
+    }
 }
 
 @end
