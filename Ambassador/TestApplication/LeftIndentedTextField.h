579b162dc2b400ffb4bae940abf37e3ac20ae0ef
jakedunahee <jake@getambassador.com>
2016-03-18 14:24:23 -0400

    [#115071307] Automatically handled indentation on textfields

---
+++ Ambassador/TestApplication/LeftIndentedTextField.h
@@ -0,0 +1,13 @@
+//
+//  LeftIndentedTextField.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/18/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeftIndentedTextField : UITextField
+
+@end
