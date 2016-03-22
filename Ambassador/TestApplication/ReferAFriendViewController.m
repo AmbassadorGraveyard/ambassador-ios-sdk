c62bf7f6a15ebdef36a6744eeaa05e76cef1a3ac
jakedunahee <jake@getambassador.com>
2016-03-22 12:45:06 -0400

    [#115528069] Implemented new RAF creation feature

--- Ambassador/TestApplication/ReferAFriendViewController.m
+++ Ambassador/TestApplication/ReferAFriendViewController.m
@@ -9,17 +9,20 @@
 #import "ReferAFriendViewController.h"
 #import "RAFCell.h"
 #import <Ambassador/Ambassador.h>
+#import "ThemeHandler.h"
+#import "DefaultsHandler.h"
 
-@interface ReferAFriendViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
+@interface ReferAFriendViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIAlertViewDelegate>
 
 // IBOutlets
 @property (nonatomic, strong) IBOutlet UIView * imgBGView;
 @property (nonatomic, strong) IBOutlet UITableView * rafTable;
 
 // Private properties
-@property (nonatomic, strong) NSArray * rafNameArray;
+@property (nonatomic, strong) NSArray * rafArray;
 
 @end
+
 
 @implementation ReferAFriendViewController
 
@@ -29,7 +32,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupUI];
-    self.rafNameArray = @[@"Ambassador Shoes RAF", @"Ambassador RAF", @"Ambassador Shirt RAF", @"Custom Branded RAF"];
+    self.rafArray = [DefaultsHandler getThemeArray];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -44,7 +47,10 @@
 #pragma mark - Button Actions
 
 - (void)addNewRAF {
-    // TODO: Add customize RAF page
+    // TODO: Replace with presenting RAF customizer
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Create Theme" message:@"Type your theme name below." delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Save", nil];
+    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    [alert show];
 }
 
 - (void)editRAF {
@@ -55,17 +61,18 @@
 #pragma mark - TableView DataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 4;
+    return self.rafArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     RAFCell *rafCell = [tableView dequeueReusableCellWithIdentifier:@"rafCell"];
     
     if (rafCell) {
-        [rafCell setUpCellWithRafName:self.rafNameArray[indexPath.row]];
+        [rafCell setUpCellWithRaf:self.rafArray[indexPath.row]];
         return rafCell;
     }
     
+    // If there is no cell, a blank cell is created so that the cell separator is not shown
     UITableViewCell *blankCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
     return blankCell;
 }
@@ -88,22 +95,9 @@
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     NSString *campaignId = @"1026"; // TEMPORARY
     
-    switch (indexPath.row) {
-        case 0:
-            [AmbassadorSDK presentRAFForCampaign:campaignId FromViewController:self withThemePlist:@"AmbassadorShoes"];
-            break;
-        case 1:
-            [AmbassadorSDK presentRAFForCampaign:campaignId FromViewController:self withThemePlist:@"AmbassadorTheme"];
-            break;
-        case 2:
-            [AmbassadorSDK presentRAFForCampaign:campaignId FromViewController:self withThemePlist:@"AmbassadorShirt"];
-            break;
-        case 3:
-            [AmbassadorSDK presentRAFForCampaign:campaignId FromViewController:self withThemePlist:@"CustomBrandTheme"];
-            break;
-        default:
-            break;
-    }
+    // Gets RAFItem at array and presents a raf using the plist name value
+    RAFItem *item = self.rafArray[indexPath.row];
+    [AmbassadorSDK presentRAFForCampaign:campaignId FromViewController:self withThemePlist:item.plistFullName];
 }
 
 
@@ -112,6 +106,17 @@
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     [textField resignFirstResponder];
     return YES;
+}
+
+
+#pragma mark - AlertView Delegate
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    // If user clicks save
+    if (buttonIndex == 1) {
+        NSString *themeName = [[alertView textFieldAtIndex:0] text];
+        [self saveNewTheme:themeName];
+    }
 }
 
 
@@ -131,6 +136,28 @@
     
     UIBarButtonItem *btnEdit = [[UIBarButtonItem alloc] initWithTitle:@"Edit" style:UIBarButtonItemStylePlain target:self action:@selector(editRAF)];
     self.tabBarController.navigationItem.leftBarButtonItem = btnEdit;
+}
+
+
+#pragma mark - Helper Functions
+
+- (void)reloadThemes {
+    self.rafArray = [DefaultsHandler getThemeArray];
+    [self.rafTable reloadData];
+}
+
+- (void)saveNewTheme:(NSString*)themeName {
+    // Creates a new RAF item which will be stored to defaults
+    RAFItem *item = [[RAFItem alloc] init];
+    item.rafName = themeName;
+    item.plistFullName = [NSString stringWithFormat:@"AMBTESTAPP%@", themeName];
+    item.dateCreated = [NSDate date];
+    
+    // Saves a new plist item using the RAF item name
+    ThemeHandler *handler = [[ThemeHandler alloc] init];
+    [handler saveNewTheme:item];
+    
+    [self reloadThemes];
 }
 
 @end
