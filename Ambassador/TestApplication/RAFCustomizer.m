9dec776dd69a992cfb6c3906e7ecaf2070fc7e08
jakedunahee <jake@getambassador.com>
2016-03-23 17:28:05 -0400

    [#115071567] Added RAFCustomizer class

---
+++ Ambassador/TestApplication/RAFCustomizer.m
@@ -0,0 +1,13 @@
+//
+//  RAFCustomizer.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/23/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import "RAFCustomizer.h"
+
+@implementation RAFCustomizer
+
+@end
