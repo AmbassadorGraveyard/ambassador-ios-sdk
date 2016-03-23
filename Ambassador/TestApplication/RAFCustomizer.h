2b2af7f7cbd01d3f8e34d5d0188b4d517f38414e
jakedunahee <jake@getambassador.com>
2016-03-23 17:28:05 -0400

    [#115071567] Added RAFCustomizer class

---
+++ Ambassador/TestApplication/RAFCustomizer.h
@@ -0,0 +1,13 @@
+//
+//  RAFCustomizer.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/23/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RAFCustomizer : UIViewController
+
+@end
