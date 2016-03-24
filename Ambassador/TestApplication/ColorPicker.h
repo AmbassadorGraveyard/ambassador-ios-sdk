061178557670db37c79519594489e7f6749b5aab
jakedunahee <jake@getambassador.com>
2016-03-24 17:00:28 -0400

    [#115071567] Added color picker class and storyboard

---
+++ Ambassador/TestApplication/ColorPicker.h
@@ -0,0 +1,13 @@
+//
+//  ColorPicker.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/24/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorPicker : UIViewController
+
+@end
---
+++ Ambassador/TestApplication/ColorPicker.m
@@ -0,0 +1,13 @@
+//
+//  ColorPicker.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/24/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import "ColorPicker.h"
+
+@implementation ColorPicker
+
+@end
