817bc6d0575cc0745197fb9eceb1f0a34cf3762d
jakedunahee <jake@getambassador.com>
2016-03-25 12:00:32 -0400

    [#115071567] Added CampaignObject class

---
+++ Ambassador/TestApplication/CampaignObject.h
@@ -0,0 +1,13 @@
+//
+//  CampaignObject.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/25/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CampaignObject : NSObject
+
+@end
---
+++ Ambassador/TestApplication/CampaignObject.m
@@ -0,0 +1,13 @@
+//
+//  CampaignObject.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/25/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import "CampaignObject.h"
+
+@implementation CampaignObject
+
+@end
