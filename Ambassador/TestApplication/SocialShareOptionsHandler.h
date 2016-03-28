474578533240f3c6788928cdd0452c5dd336d6e0
jakedunahee <jake@getambassador.com>
2016-03-28 16:20:57 -0400

    [#115071567] Added class to handle social table

---
+++ Ambassador/TestApplication/SocialShareOptionsHandler.h
@@ -0,0 +1,13 @@
+//
+//  SocialShareOptionsHandler.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/28/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocialShareOptionsHandler : NSObject
+
+@end
