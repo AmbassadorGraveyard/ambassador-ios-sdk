97b4a3f234993e98ed2b20e7884e8af02b59797b
jakedunahee <jake@getambassador.com>
2016-03-29 09:16:40 -0400

    [#115071567] Added SocialCell class

---
+++ Ambassador/TestApplication/SocialCell.h
@@ -0,0 +1,13 @@
+//
+//  SocialCell.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/28/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialCell : UITableViewCell
+
+@end
---
+++ Ambassador/TestApplication/SocialCell.m
@@ -0,0 +1,13 @@
+//
+//  SocialCell.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/28/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import "SocialCell.h"
+
+@implementation SocialCell
+
+@end
