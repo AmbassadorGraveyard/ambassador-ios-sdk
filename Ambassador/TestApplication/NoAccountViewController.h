a6e58b1d66a4c99e3aa7aa7859d745132a6718f7
jakedunahee <jake@getambassador.com>
2016-03-30 14:22:55 -0400

    [#116247489] Added ViewController for 'Dont have an account button'

---
+++ Ambassador/TestApplication/NoAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  NoAccountViewController.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 3/30/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoAccountViewController : UIViewController
+
+@end
