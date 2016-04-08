cb41f283a745f479adf976d9109f055b7cd45db5
jakedunahee <jake@getambassador.com>
2016-04-08 09:50:32 -0400

    [#115967953] Added NPS ViewController

---
+++ Ambassador/Ambassador/AMBNPSViewController.h
@@ -0,0 +1,13 @@
+//
+//  AMBNPSViewController.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 4/7/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMBNPSViewController : UIViewController
+
+@end
