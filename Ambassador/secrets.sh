453a40cd551d93f6755500867aecb72b62fcde4a
jakedunahee <jake@getambassador.com>
2016-04-12 15:20:16 -0400

    [#115970351] Used cd instead

--- Ambassador/secrets.sh
+++ Ambassador/secrets.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd Ambassador
 echo "<?xml version="1.0" encoding="UTF-8"?>" >> AmbassadorSecrets.plist
 echo "<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">" >> AmbassadorSecrets.plist
 echo "<plist version="1.0">" >> AmbassadorSecrets.plist
@@ -12,4 +13,3 @@ echo "    <key>SENTRY_KEY</key>" >> AmbassadorSecrets.plist
 echo "    <string>$SENTRY_KEY</string>" >> AmbassadorSecrets.plist
 echo "</dict>" >> AmbassadorSecrets.plist
 echo "</plist>" >> AmbassadorSecrets.plist
-mv /AmbassadorSecrets.plist /Ambassador/
