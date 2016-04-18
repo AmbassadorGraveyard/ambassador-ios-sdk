fc26f62f16174f1af4d10ab425cfefa7ed41a169
jakedunahee <jake@getambassador.com>
2016-04-18 18:06:23 -0400

    [#117480569] Added slidingView class

---
+++ Ambassador/TestApplication/SlidingView.h
@@ -0,0 +1,13 @@
+//
+//  SlidingView.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 4/18/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlidingView : UIView
+
+@end
---
+++ Ambassador/TestApplication/SlidingView.m
@@ -0,0 +1,21 @@
+//
+//  SlidingView.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 4/18/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import "SlidingView.h"
+
+@implementation SlidingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
