9c42d84f1c3f992f36be5b529b51cd040fb1fe18
jakedunahee <jake@getambassador.com>
2016-04-25 14:10:56 -0400

    [#118047863] Added enum for README types

--- Ambassador/TestApplication/FileWriter.h
+++ Ambassador/TestApplication/FileWriter.h
@@ -8,12 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum readMeType {
+    ReadmeTypeIdentify,
+    ReadmeTypeConversion,
+    ReadmeTypeRAF
+} READMETypes;
+
 @interface FileWriter : NSObject
 
 + (NSString *)objcAppDelegateFileWithInsert:(NSString *)insert;
 + (NSString *)swiftAppDelegateFileWithInsert:(NSString *)insert;
 + (NSString *)javaMyApplicationFileWithInsert:(NSString *)insert;
-+ (NSString *)readMeForRequest:(NSString *)requestName;
++ (NSString *)readMeForRequest:(READMETypes)readmeType;
 + (NSString *)objcViewControllerWithInsert:(NSString *)insert;
 + (NSString *)swiftViewControllerWithInsert:(NSString *)insert;
 + (NSString *)javaActivityWithInsert:(NSString *)insert;
