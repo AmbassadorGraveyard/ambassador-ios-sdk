d4c5b46c3790e127a7d5287631567088ae385f6d
jakedunahee <jake@getambassador.com>
2016-05-02 09:29:01 -0400

    [#117187291] Added SurverySlider class

---
+++ Ambassador/Ambassador/AMBSurveySlider.h
@@ -0,0 +1,13 @@
+//
+//  AMBSurveySlider.h
+//  Ambassador
+//
+//  Created by Jake Dunahee on 4/29/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMBSurveySlider : NSObject
+
+@end
---
+++ Ambassador/Ambassador/AMBSurveySlider.m
@@ -0,0 +1,13 @@
+//
+//  AMBSurveySlider.m
+//  Ambassador
+//
+//  Created by Jake Dunahee on 4/29/16.
+//  Copyright © 2016 Ambassador. All rights reserved.
+//
+
+#import "AMBSurveySlider.h"
+
+@implementation AMBSurveySlider
+
+@end
