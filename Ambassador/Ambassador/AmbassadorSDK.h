c004137124f104d1f1a87a04581e8b8bc10bb757
jakedunahee <jake@getambassador.com>
2016-05-10 11:47:48 -0400

    [#119047655] Added 'apple docs' documentation for each function in the AmbassadorSDK.h file

--- Ambassador/Ambassador/AmbassadorSDK.h
+++ Ambassador/Ambassador/AmbassadorSDK.h
@@ -10,38 +10,125 @@
 
 @interface AmbassadorSDK : NSObject
 
-/**Initializes Ambassador in an application using the provided credentials*/
+/**
+ 
+ Initializes the Ambassador SDK in an application using the provided credentials.
+ 
+ Recommended to put inside of the AppDelegate's 'didFinishLaunchingWithOptions:' function to initialize Ambassador as soon as possible.
+ 
+ @param universalToken Token provided by Ambassador which gives access to use Ambassador's SDK.
+ @param universalID An ID provided by Ambassador which gives access to use Ambassador's SDK.
+ 
+ */
 + (void)runWithUniversalToken:(NSString *)UniversalToken universalID:(NSString *)universalID;
 
-/**Identifies a user and associates it with the given email*/
+
+/**
+ 
+ Identifies a user and associates it with the given email.
+ 
+ Recommended to put on a login screen or after the initial call to run Ambassador if you have the user's email stored.
+ 
+ @param email User's email address which is used to identify a user.
+ 
+ @warning This method is deprecated as of version 1.0.4.
+ 
+ */
 + (void)identifyWithEmail:(NSString *)email __attribute__((deprecated("Use method 'identifyWithUserID: traits:' instead")));
 
+
 /** 
+ 
  Identifies a user based a unique userID and a dictionary of traits.
  
  Recommended to put on a login screen or after the initial call to run Ambassador if you have the user's info stored.
  
  @param userID A unique ID tied to the user being identified
  @param traits Extra values tied to the user. Ex: Email, first name, last name
-*/
+ 
+ @warning It is highly recommended to at least include an 'email' value in the traits dictionary in order for full functionality in the Ambassador SDK.
+ 
+ */
 + (void)identifyWithUserID:(NSString *)userID traits:(NSDictionary *)traits;
 
-/**Registers a conversion with Ambassador using the properties set in 'conversionParameters'*/
+
+/**
+ 
+ Registers a conversion with Ambassador.
+ 
+ @param conversionParameters The object used to set all of the values for a specific conversion.
+ @param restrictToInstall Boolean value that decides where the conversion should only be allowed to get called once.
+ @param completion Block that tells the user when the conversion is done attempting to send.
+ 
+ */
 + (void)registerConversion:(AMBConversionParameters *)conversionParameters restrictToInstall:(BOOL)restrictToInstall completion:(void (^)(NSError *error))completion;
 
-/**Presents a full-page modal 'Refer-A-Friend' (RAF) ViewController*/
+
+/**
+ 
+ Presents a full-page modal 'Refer-A-Friend' (RAF) view-controller.
+ 
+ It is important that the view-controller being passed is in the view hierarchy before the call is made.
+ 
+ @param ID The campaign's ID that is tied to the RAF.
+ @param viewController The view-controller which is presenting the RAF.
+ @param themePlist An optional parameter which tells the RAF which theme to use when presenting.  If nil, Ambassador's generic theme will be used.
+ 
+ @warning Calling this function in a view-controller's 'viewDidLoad' or 'viewWillAppear' functions may result in failure.
+ 
+ */
 + (void)presentRAFForCampaign:(NSString *)ID FromViewController:(UIViewController *)viewController withThemePlist:(NSString*)themePlist;
 
-/**Registers a device's APN Device Token in order to send notifications*/
+
+/**
+ 
+ Registers a device's APN Device Token in order to send notifications.
+ 
+ @param deviceToken Apple Push Notification device token which can be found in the AppDelegate's 'didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken' function if your app uses Apple Push Notifications.
+ 
+ */
 + (void)registerDeviceToken:(NSString*)deviceToken;
 
-/**Handles remote notifications that are sent from Ambassador**/
+
+/**
+ 
+ Presents a Net Promoter Score (NPS) survey when triggered by a Push Notification.
+ 
+ This function should be called from the AppDelegate's 'didReceiveRemoteNotification:(NSDictionary *)userInfo' function.
+ Put an 'if (userInfo[@"ambassador_sdk"] && [userInfo[@"ambassador_sdk"] boolValue])' statement around this function to guaruntee that the notificaiton is intended for Ambassador.
+ 
+ @param notification The notification body sent through the AppDelegate's 'didReceiveRemoteNotification:(NSDictionary *)userInfo' function.
+ 
+ **/
 + (void)presentNPSSurveyWithNotification:(NSDictionary*)notification;
 
-/**Presents NPS Survey from notification with a custom theme*/
+
+/**
+ 
+ Presents a Net Promoter Score (NPS) survey based on a Push Notification using a custom theme.
+ 
+ This function should be called from the AppDelegate's 'didReceiveRemoteNotification:(NSDictionary *)userInfo' function.
+ Put an 'if (userInfo[@"ambassador_sdk"] && [userInfo[@"ambassador_sdk"] boolValue])' statement around this function to guaruntee that the notificaiton is intended for Ambassador.
+ 
+ @param notification The notification body sent through the AppDelegate's 'didReceiveRemoteNotification:(NSDictionary *)userInfo' function.
+ @param backgroundColor The background color of the NPS survey.
+ @param contentColor The color used for the text and the NPS score slider.
+ @param buttonColor The background color of the 'Submit' button in the NPS survey.
+ 
+ */
 + (void)presentNPSSurveyWithNotification:(NSDictionary *)notification backgroundColor:(UIColor *)backgroundColor contentColor:(UIColor *)contentColor buttonColor:(UIColor *)buttonColor;
 
-/**Generates a Welcome Screen ViewController based on referrer's data, which can be handled by the caller*/
+
+/**
+ 
+ Creates a Welcome Screen view-controller based on an Ambassador's data.
+ 
+ The Welcome Screen will only be available if the app was installed from an Ambassador referral.
+ 
+ @param parameters An object that contains the parameters used to customize the Welcome Screen.
+ @param available A block that gets called if a referral took place and passes back the AMBWelcomeScreenViewController to be handled.
+ 
+ */
 + (void)presentWelcomeScreen:(AMBWelcomeScreenParameters*)parameters ifAvailable:(void(^)(AMBWelcomeScreenViewController *welcomeScreenVC))available;
 
 @end
