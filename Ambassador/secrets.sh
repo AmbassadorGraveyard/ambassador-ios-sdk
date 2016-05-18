26fa36d147fe5b78ad94849a56db3a008d73007a
jakedunahee <jake@getambassador.com>
2016-05-18 14:51:27 -0400

    [#119844165] Updated secrets script

--- Ambassador/secrets.sh
+++ Ambassador/secrets.sh
@@ -6,30 +6,22 @@ echo #import "AMBSecrets.h"
 
 echo @implementation AMBSecrets
 
-echo NSString const * PUSHER_DEV_KEY = @"$PUSHER_DEV_KEY";
-echo NSString const * PUSHER_PROD_KEY = @"$PUSHER_PROD_KEY";
-echo NSString const * SENTRY_KEY = @"$SENTRY_KEY";
-
-echo + (NSDictionary *)secretsDictionary {
-echo     NSBundle *ambassadorBundle = [NSBundle bundleForClass:[self class]];
-echo     NSString *secretsPath = [ambassadorBundle pathForResource:@"AmbassadorSecrets" ofType:@"plist"];
-    
-echo     return [NSDictionary dictionaryWithContentsOfFile:secretsPath];
-echo }
+echo NSString * PUSHER_DEV_KEY = @"$PUSHER_DEV_KEY";
+echo NSString * PUSHER_PROD_KEY = @"$PUSHER_PROD_KEY";
+echo NSString * SENTRY_KEY = @"$SENTRY_KEY";    
 
 echo + (NSString *)secretForKey:(AMBSecretKeys)key {
-echo     NSDictionary *secretsDict = [AMBSecrets secretsDictionary];
-echo     NSString *dictKey = [AMBSecrets stringForKey:key];
-echo     NSString *returnString = secretsDict[dictKey] != nil ? secretsDict[dictKey] : @"Unavailable";
+echo     NSString *keyString = [self stringForKey:key];
+echo     NSString *returnString = keyString != nil ? keyString : @"Unavailable";
     
 echo     return returnString;
 echo }
 
 echo + (NSString *)stringForKey:(AMBSecretKeys)key {
 echo     switch (key) {
-echo         case AMB_PUSHER_DEV_KEY: return @"PUSHER_DEV_KEY";
-echo         case AMB_PUSHER_PROD_KEY: return @"PUSHER_PROD_KEY";
-echo         case AMB_SENTRY_KEY: return @"SENTRY_KEY";
+echo         case AMB_PUSHER_DEV_KEY: return PUSHER_DEV_KEY;
+echo         case AMB_PUSHER_PROD_KEY: return PUSHER_PROD_KEY;
+echo         case AMB_SENTRY_KEY: return SENTRY_KEY;
 echo         default: return @"Unavailable";
 echo     }
 echo }
