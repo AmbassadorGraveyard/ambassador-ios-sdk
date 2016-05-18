51c29e6a464986bd8cdf2d4a9ab884c245f66200
jakedunahee <jake@getambassador.com>
2016-05-18 15:12:51 -0400

    [#119844165] Added proper escape slashes

--- Ambassador/secrets.sh
+++ Ambassador/secrets.sh
@@ -2,17 +2,17 @@
 
 cd Ambassador
 
-echo " #import "AMBSecrets.h"" >> AMBSecrets.m
+echo " #import \"AMBSecrets.h\"" >> AMBSecrets.m
 
 echo "@implementation AMBSecrets" >> AMBSecrets.m
 
-echo "NSString * PUSHER_DEV_KEY = @"$PUSHER_DEV_KEY";" >> AMBSecrets.m
-echo "NSString * PUSHER_PROD_KEY = @"$PUSHER_PROD_KEY";" >> AMBSecrets.m
-echo "NSString * SENTRY_KEY = @"$SENTRY_KEY";" >> AMBSecrets.m
+echo "NSString * PUSHER_DEV_KEY = @\"$PUSHER_DEV_KEY\";" >> AMBSecrets.m
+echo "NSString * PUSHER_PROD_KEY = @\"$PUSHER_PROD_KEY\";" >> AMBSecrets.m
+echo "NSString * SENTRY_KEY = @\"$SENTRY_KEY\";" >> AMBSecrets.m
 
 echo "+ (NSString *)secretForKey:(AMBSecretKeys)key {" >> AMBSecrets.m
 echo "    NSString *keyString = [self stringForKey:key];" >> AMBSecrets.m
-echo "    NSString *returnString = keyString != nil ? keyString : @"Unavailable";" >> AMBSecrets.m
+echo "    NSString *returnString = keyString != nil ? keyString : @\"Unavailable\";" >> AMBSecrets.m
     
 echo "    return returnString;" >> AMBSecrets.m
 echo "}" >> AMBSecrets.m
