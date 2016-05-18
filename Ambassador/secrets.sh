d622b19273f2080adaf9aa35b4321b3968215db0
jakedunahee <jake@getambassador.com>
2016-05-18 15:05:44 -0400

    [#119844165] Added proper quotes and syntax to bash script

--- Ambassador/secrets.sh
+++ Ambassador/secrets.sh
@@ -2,28 +2,28 @@
 
 cd Ambassador
 
-echo #import "AMBSecrets.h"
+echo " #import "AMBSecrets.h"" >> AMBSecrets.m
 
-echo @implementation AMBSecrets
+echo "@implementation AMBSecrets" >> AMBSecrets.m
 
-echo NSString * PUSHER_DEV_KEY = @"$PUSHER_DEV_KEY";
-echo NSString * PUSHER_PROD_KEY = @"$PUSHER_PROD_KEY";
-echo NSString * SENTRY_KEY = @"$SENTRY_KEY";    
+echo "NSString * PUSHER_DEV_KEY = @"$PUSHER_DEV_KEY";" >> AMBSecrets.m
+echo "NSString * PUSHER_PROD_KEY = @"$PUSHER_PROD_KEY";" >> AMBSecrets.m
+echo "NSString * SENTRY_KEY = @"$SENTRY_KEY";" >> AMBSecrets.m
 
-echo + (NSString *)secretForKey:(AMBSecretKeys)key {
-echo     NSString *keyString = [self stringForKey:key];
-echo     NSString *returnString = keyString != nil ? keyString : @"Unavailable";
+echo "+ (NSString *)secretForKey:(AMBSecretKeys)key {" >> AMBSecrets.m
+echo "    NSString *keyString = [self stringForKey:key];" >> AMBSecrets.m
+echo "    NSString *returnString = keyString != nil ? keyString : @"Unavailable";" >> AMBSecrets.m
     
-echo     return returnString;
-echo }
+echo "    return returnString;" >> AMBSecrets.m
+echo "}" >> AMBSecrets.m
 
-echo + (NSString *)stringForKey:(AMBSecretKeys)key {
-echo     switch (key) {
-echo         case AMB_PUSHER_DEV_KEY: return PUSHER_DEV_KEY;
-echo         case AMB_PUSHER_PROD_KEY: return PUSHER_PROD_KEY;
-echo         case AMB_SENTRY_KEY: return SENTRY_KEY;
-echo         default: return @"Unavailable";
-echo     }
-echo }
+echo "+ (NSString *)stringForKey:(AMBSecretKeys)key {" >> AMBSecrets.m
+echo "    switch (key) {" >> AMBSecrets.m
+echo "        case AMB_PUSHER_DEV_KEY: return PUSHER_DEV_KEY;" >> AMBSecrets.m
+echo "        case AMB_PUSHER_PROD_KEY: return PUSHER_PROD_KEY;" >> AMBSecrets.m
+echo "        case AMB_SENTRY_KEY: return SENTRY_KEY;" >> AMBSecrets.m
+echo "        default: return @"Unavailable";" >> AMBSecrets.m
+echo "    }" >> AMBSecrets.m
+echo "}" >> AMBSecrets.m
 
-echo @end
+echo "@end" >> AMBSecrets.m
