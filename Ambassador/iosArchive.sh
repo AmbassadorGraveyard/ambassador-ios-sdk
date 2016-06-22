62ac0bdce6f49f504822cb7819a998f89a9cec23
jakedunahee <jake@getambassador.com>
2016-06-22 10:13:30 -0400

    Changed from proj to wc

--- Ambassador/iosArchive.sh
+++ Ambassador/iosArchive.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-xcodebuild archive -project Ambassador.xcworkspace -scheme DemoApp -archivePath Ambassador.xcarchive
+xcodebuild archive -workspace Ambassador.xcworkspace -scheme DemoApp -archivePath Ambassador.xcarchive
 
 xcodebuild -exportArchive -archivePath Ambassador.xcarchive -exportPath $CIRCLE_ARTIFACTS/IPAs/Ambassador.ipa -exportFormat ipa -exportProvisioningProfile "Ambassador SDK Application"
