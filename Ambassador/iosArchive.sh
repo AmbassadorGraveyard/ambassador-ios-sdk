db2ac063174664eb048f01c9796dfeb15356a0ac
jakedunahee <jake@getambassador.com>
2016-06-22 10:55:16 -0400

    Trying to fix circle build

--- Ambassador/iosArchive.sh
+++ Ambassador/iosArchive.sh
@@ -2,4 +2,4 @@
 
 xcodebuild archive -workspace Ambassador.xcworkspace -scheme DemoApp -archivePath Ambassador.xcarchive
 
-xcodebuild -exportArchive -archivePath Ambassador.xcarchive -exportPath $CIRCLE_ARTIFACTS/IPAs/Ambassador.ipa -exportFormat ipa
+xcodebuild -exportArchive -archivePath Ambassador.xcarchive -exportPath $CIRCLE_ARTIFACTS/IPAs/Ambassador.ipa -exportFormat ipa -exportProvisioningProfile "Ambassador SDK Application"
