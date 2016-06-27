0c41d03680c58152701689ee6bb8bc6bb67882f4
jakedunahee <jake@getambassador.com>
2016-06-27 14:53:14 -0400

    [#122541171] Added derivedDataPath to build

--- Ambassador/buildFramework.sh
+++ Ambassador/buildFramework.sh
@@ -2,7 +2,7 @@
 
 #if [ "$CIRCLE_BRANCH" == *"release"*]
  #  then
-      mkdir $CIRCLE_ARTIFACTS/Framework_files
-      xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework -archivePath $CIRCLE_ARTIFACTS/test.txt clean build 
+      #mkdir $CIRCLE_ARTIFACTS/Framework_files
+      xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework -derivedDataPath $CIRCLE_ARTIFACTS clean build 
       #cp Users/distiller/Library/Developer/Xcode/DerivedData/ $CIRCLE_ARTIFACTS/Framework_files
 #fi
