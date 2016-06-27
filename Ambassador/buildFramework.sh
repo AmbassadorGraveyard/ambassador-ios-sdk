58d52fc604bff0384a195cda74d90ec6e00442ee
jakedunahee <jake@getambassador.com>
2016-06-27 12:58:18 -0400

    [#122541171] Another test run with fixed path

--- Ambassador/buildFramework.sh
+++ Ambassador/buildFramework.sh
@@ -4,5 +4,5 @@
  #  then
       xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework clean build 
       mkdir $CIRCLE_ARTIFACTS/Framework_files
-      cp ~/Library/Developer/Xcode/DerivedData/ $CIRCLE_ARTIFACTS/Framework_files
+      cp Users/distiller/Library/Developer/Xcode/DerivedData/Ambassador-besyrlrkrscgnsfebardmtdyiqvx/Build/Intermediates/Ambassador.build/Release-iphoneos/Framework.build $CIRCLE_ARTIFACTS/Framework_files
 #fi
