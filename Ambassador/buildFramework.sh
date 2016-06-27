8897439d438a06565e63cccee624b89057d9564a
jakedunahee <jake@getambassador.com>
2016-06-27 17:02:57 -0400

    [#122541171] Updated bash script and reverted yml

--- Ambassador/buildFramework.sh
+++ Ambassador/buildFramework.sh
@@ -4,12 +4,6 @@ y#!/bin/sh
  #  then
       #mkdir $CIRCLE_ARTIFACTS/Framework_files
       xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework -derivedDataPath DerivedDataclean build 
-     
-      for file in /Users/distiller/ambassador-ios-sdk/Ambassador/DerivedDataclean/Ambassador/Build/Products/Release-iphoneos/*; do
-        echo ${file##*/} 
-      done
-
-      echo $(basename /*)
-
-      cp /Users/distiller/ambassador-ios-sdk/Ambassador/DerivedDataclean/Build/Products/Release-iphoneos/Ambassador.framework $CIRCLE_ARTIFACTS/Ambassador.framework
+      mv /Users/distiller/ambassador-ios-sdk/Ambassador/DerivedData/Build/Products/Release-iphoneos/Ambassador.framework $CIRCLE_ARTIFACTS/Ambassador.framework
+      mv /Users/distiller/ambassador-ios-sdk/Ambassador/DerivedData/Build/Products/Release-iphoneos/Ambassador.bundle $CIRCLE_ARTIFACTS/Ambassador.bundle
 #fi
