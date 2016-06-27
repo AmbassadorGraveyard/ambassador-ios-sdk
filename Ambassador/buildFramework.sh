90f2e6f129e711be08b4df7a63d8d1a02debf5e4
jakedunahee <jake@getambassador.com>
2016-06-27 11:46:39 -0400

    [#122541171] Temp removed 'release' check for testing

--- Ambassador/buildFramework.sh
+++ Ambassador/buildFramework.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-if [ "$CIRCLE_BRANCH" == *"release"*]
-   then
+#if [ "$CIRCLE_BRANCH" == *"release"*]
+ #  then
       xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework clean build 
       mkdir $CIRCLE_ARTIFACTS/Framework_files
       cp ~/Library/Developer/Xcode/DerivedData/ $CIRCLE_ARTIFACTS/Framework_files
-fi
-
-echo $CIRCLE_SHA1
+#fi
