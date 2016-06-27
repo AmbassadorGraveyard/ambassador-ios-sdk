56f6048e660b831c0cbe7ce89ed9483a6d762cd7
jakedunahee <jake@getambassador.com>
2016-06-27 13:43:42 -0400

    [#122541171] User archivePath option

--- Ambassador/buildFramework.sh
+++ Ambassador/buildFramework.sh
@@ -2,7 +2,7 @@
 
 #if [ "$CIRCLE_BRANCH" == *"release"*]
  #  then
-      xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework clean build 
       mkdir $CIRCLE_ARTIFACTS/Framework_files
-      cp Users/distiller/Library/Developer/Xcode/DerivedData/ $CIRCLE_ARTIFACTS/Framework_files
+      xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework -archivePath $CIRCLE_ARTIFACTS/Framework_files clean build 
+      #cp Users/distiller/Library/Developer/Xcode/DerivedData/ $CIRCLE_ARTIFACTS/Framework_files
 #fi
