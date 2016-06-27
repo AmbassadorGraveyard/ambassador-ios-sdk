817da8ae31b567879daeca6a9407256613b77139
jakedunahee <jake@getambassador.com>
2016-06-27 11:02:00 -0400

    [#122541171] Updated circle.yml

--- Ambassador/buildFramework.sh
+++ Ambassador/buildFramework.sh
@@ -2,7 +2,7 @@
 
 if [ "$CIRCLE_BRANCH" == *"@build_framework"*]
    then
-      xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=9.2,name=iPhone 6' -configuration Release -scheme Framework clean build 
+      xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework clean build 
       mkdir $CIRCLE_ARTIFACTS/Framework_files
       cp ~/Library/Developer/Xcode/DerivedData/ $CIRCLE_ARTIFACTS/Framework_files
 fi
