91e1e1df66946da8a13cdd5641925bd156f41355
jakedunahee <jake@getambassador.com>
2016-06-27 16:06:16 -0400

    [#122541171] Added file printing

--- Ambassador/buildFramework.sh
+++ Ambassador/buildFramework.sh
@@ -4,5 +4,10 @@
  #  then
       #mkdir $CIRCLE_ARTIFACTS/Framework_files
       xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework -derivedDataPath DerivedDataclean build 
+     
+      for file in /DerivedData/Ambassador/Build/Products/Release-iphoneos/*; do
+        echo ${file##*/} 
+      done
+
       cp /DerivedData/Ambassador/Build/Products/Release-iphoneos/Ambassador.framework $CIRCLE_ARTIFACTS/Ambassador.framework
 #fi
