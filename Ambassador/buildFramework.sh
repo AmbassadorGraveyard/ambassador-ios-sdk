596ac9f6c5c8ce2706e0b03a95423c3910d04d13
jakedunahee <jake@getambassador.com>
2016-06-27 15:10:28 -0400

    [#122541171] Attempting to rewrite derivedData path

--- Ambassador/buildFramework.sh
+++ Ambassador/buildFramework.sh
@@ -3,6 +3,6 @@
 #if [ "$CIRCLE_BRANCH" == *"release"*]
  #  then
       #mkdir $CIRCLE_ARTIFACTS/Framework_files
-      xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework -derivedDataPath $CIRCLE_ARTIFACTS clean build 
-      #cp Users/distiller/Library/Developer/Xcode/DerivedData/ $CIRCLE_ARTIFACTS/Framework_files
+      xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework -derivedDataPath build clean build 
+      cp build/Build/Products/Release-iphoneos/Ambassador.framework $CIRCLE_ARTIFACTS/Ambassador.framework
 #fi
