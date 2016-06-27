179bd54c22c3e46e426a62cf67c0edabe1a6cb82
jakedunahee <jake@getambassador.com>
2016-06-27 15:22:29 -0400

    [#122541171] Hopefully fixed directory

--- Ambassador/buildFramework.sh
+++ Ambassador/buildFramework.sh
@@ -4,5 +4,5 @@
  #  then
       #mkdir $CIRCLE_ARTIFACTS/Framework_files
       xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework -derivedDataPath build clean build 
-      cp build/Build/Products/Release-iphoneos/Ambassador.framework $CIRCLE_ARTIFACTS/Ambassador.framework
+      mv /Users/distiller/ambassdor-ios-sdk/build/Build/Products/Release-iphoneos/Ambassador.framework $CIRCLE_ARTIFACTS/Ambassador.framework
 #fi
