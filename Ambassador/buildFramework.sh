bf55e796a856c2aa3c30b4d99a83d73af2a0a001
jakedunahee <jake@getambassador.com>
2016-06-28 09:16:19 -0400

    [#122541171] Removed buildFramework script

--- Ambassador/buildFramework.sh
+++
@@ -1,9 +0,0 @@
-y#!/bin/sh
-
-#if [ "$CIRCLE_BRANCH" == *"release"*]
- #  then
-      #mkdir $CIRCLE_ARTIFACTS/Framework_files
-      xcodebuild -workspace Ambassador.xcworkspace -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' -configuration Release -scheme Framework -derivedDataPath DerivedDataclean build 
-      mv /Users/distiller/ambassador-ios-sdk/Ambassador/DerivedData/Build/Products/Release-iphoneos/Ambassador.framework $CIRCLE_ARTIFACTS/Ambassador.framework
-      mv /Users/distiller/ambassador-ios-sdk/Ambassador/DerivedData/Build/Products/Release-iphoneos/Ambassador.bundle $CIRCLE_ARTIFACTS/Ambassador.bundle
-#fi
