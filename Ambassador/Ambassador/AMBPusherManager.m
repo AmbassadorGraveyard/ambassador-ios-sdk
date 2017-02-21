93eb3fba19b7b013f23f3ce8519ad2cae4bb33c3
Matt Majewski <matt@getambassador.com>
2017-02-21 08:59:11 -0500

    Add fingerprint conditional when mbsy_cookie_code doesn't exist, but fingerprint does

--- Ambassador/Ambassador/AMBPusherManager.m
+++ Ambassador/Ambassador/AMBPusherManager.m
@@ -90,7 +90,13 @@
             [AMBValues setMbsyCookieWithCode:json[@"mbsy_cookie_code"]]; // Saves mbsy cookie to defaults
             [AMBValues setDeviceFingerPrintWithDictionary:json[@"fingerprint"]]; // Saves device fp to defaults
             [[NSNotificationCenter defaultCenter] postNotificationName:@"deviceInfoReceived" object:nil];
-        } else {
+            
+        } else if (json[@"fingerprint"] && json[@"fingerprint"] != [NSNull null]) {
+            NSDictionary *consumerDict = @{@"UID" : json[@"fingerprint"][@"consumer"][@"UID"]};
+            NSDictionary *deviceDict = @{@"type" : json[@"fingerprint"][@"device"][@"type"], @"ID" : json[@"fingerprint"][@"device"][@"ID"]};
+            NSDictionary *fingerPrintDict = @{@"consumer" : consumerDict, @"device" : deviceDict };
+            [AMBValues setDeviceFingerPrintWithDictionary:fingerPrintDict]; // Saves device fp to defaults
+        }else {
             // Attempts to close socket
             [self receivedIdentifyAction];
             [user fillWithDictionary:json completion:^{
