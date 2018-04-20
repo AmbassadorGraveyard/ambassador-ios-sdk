fa5bc829a437d469070ef98e24a2e02e05013ea4
jarell-lloyd <jarell.lloyd@gmail.com>
2018-04-20 16:00:13 -0400

    automate rainforest runs

---
+++ rainforest_prod.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# fail if err occurs on any step
+set -o errexit
+
+rainforest run --run-group 1655 --fail-fast --site 8413 --token "$RAINFOREST_TOKEN" --description "Ambassador App automated post deploy run" --bg
