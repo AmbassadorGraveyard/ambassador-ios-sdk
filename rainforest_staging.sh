92d0f19ebcddc77a1ff6ff07278578b9292b3a4a
jarell-lloyd <jarell.lloyd@gmail.com>
2018-04-25 15:47:00 -0400

    updated description

--- rainforest_staging.sh
+++ rainforest_staging.sh
@@ -3,5 +3,5 @@
 # fail if err occurs on any step
 set -o errexit
 
-rainforest run --run-group 1655 --fail-fast --environment-id 5165 --token "$RAINFOREST_TOKEN" --description "Ambassador App automated post deploy run" --bg
+rainforest run --run-group 1655 --fail-fast --environment-id 5165 --token "$RAINFOREST_TOKEN" --description "Ambassador iOS Demo App automated post deploy run (Staging)" --bg
 
