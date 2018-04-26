9d15de3da40a6165bebff8b2cee865a8c3da9c23
Matt Majewski <matt@getambassador.com>
2018-04-26 08:23:08 -0400

    Add testapp

---
+++ Ambassador/SDKTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDKTestApp
+//
+//  Created by Matthew Majewski on 4/26/18.
+//  Copyright © 2018 Ambassador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ambassador/SDKTestApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SDKTestApp
+//
+//  Created by Matthew Majewski on 4/26/18.
+//  Copyright © 2018 Ambassador. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
