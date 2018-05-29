6183c0bfab8031eac00b0680efec1d1fbc9a6009
Corey Fields <corey@getambassador.com>
2018-05-29 17:18:00 -0400

    Update rainforest_prod.sh

--- rainforest_prod.sh
+++ rainforest_prod.sh
@@ -3,5 +3,5 @@
 # fail if err occurs on any step
 set -o errexit
 
-rainforest run --run-group 1655 --fail-fast --environment-id 5172 --token "$RAINFOREST_TOKEN" --description "Ambassador iOS Demo App automated post deploy run (Production)" --bg
+rainforest run --run-group 1728 --fail-fast --token "$RAINFOREST_TOKEN" --description "Ambassador iOS Demo App automated post deploy run (Production)" --bg
 
